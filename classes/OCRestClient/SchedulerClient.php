8e848a49819bd12e39ebb5c69a6d3df747635405
Andre Klassen <andre.klassen@gmail.com>
2011-11-26 13:52:00 +0100

    add body for SchedulerClient

---
+++ classes/OCRestClient/SchedulerClient.php
@@ -0,0 +1,15 @@
+<?php
+	require_once "OCRestClient.php";
+	class SchedulerClient extends OCRestClient
+	{
+		function __construct($config) {
+			if (is_array($config)) {
+				parent::__construct($config['service_url'],
+									$config['service_user'],
+									$config['service_password']);
+			} else {
+				throw new Exception (_("Die Schedulerservice Konfiguration wurde nicht im gültigen Format angegeben."));
+			}
+		}
+	}
+?>
