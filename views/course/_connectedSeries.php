1dc35fd39d3802386e109173d27f2c294136d3ee
sebwert <sewert@kerrera.(none)>
2012-11-14 10:08:39 +0100

    changed html format

--- views/course/_connectedSeries.php
+++ views/course/_connectedSeries.php
@@ -1,69 +1,69 @@
 <div>
-        <? if (false && sizeof($connectedSeries) == 0) : ?>
-            <?= MessageBox::info(_("Es sind bislang noch keine Series verfügbar. Bitte überprüfen Sie die globalen Opencast Matterhorn Einstellungen.")) ?>
-        <? else : ?>
-        <form action="<?= PluginEngine::getLink('opencast/course/edit/'.$course_id) ?>"
-            method=post>
-                    <div style="text-align: center;">
-                    <p> <?//=_("Series ohne Zuordnung")?></p>
-                    <? if(!empty ($unconnectedSeries)) :?>
+    <? if (false && sizeof($connectedSeries) == 0) : ?>
+        <?= MessageBox::info(_("Es sind bislang noch keine Series verfügbar. Bitte überprüfen Sie die globalen Opencast Matterhorn Einstellungen.")) ?>
+    <? else : ?>
+        <form action="<?= PluginEngine::getLink('opencast/course/edit/' . $course_id) ?>"
+              method=post>
+            <div style="text-align: center;">
+                <p> <? //=_("Series ohne Zuordnung") ?></p>
+                <? if (!empty($unconnectedSeries)) : ?>
                     <select class="series_select" multiple="multiple" name="series[]">
 
-                        <? foreach($unconnectedSeries as $serie) :?>
-                            <? if(isset($serie['identifier'])) : ?>
+                        <? foreach ($unconnectedSeries as $serie) : ?>
+                            <? if (isset($serie['identifier'])) : ?>
 
-                                <option value="<?=$serie['identifier']?>">
+                                <option value="<?= $serie['identifier'] ?>">
                                     <?= $serie['title'] ?>
-                                    <?//= $series->additionalMetadata?>
+                                    <? //= $series->additionalMetadata?>
                                 </option>
                             <? endif ?>
                         <? endforeach ?>
                     </select>
-         <? endif ?>
-</div>
+                <? endif ?>
+            </div>
 
 
             <div style="dislay:inline;vertical-align:middle">
                 <div style="float:left;">
-                    <p><?=_("Zugeordnete Series")?></p>
+                    <p><?= _("Zugeordnete Series") ?></p>
                     <ul style="list-style-type: none;margin:0;padding-left:50px">
-                        <? if(!empty($connectedSeries)) :?>
-                        <? foreach($connectedSeries as $serie) :?>
-                            <li>
-                                <?=  $serie['title']?>
-                                <a href="<?=PluginEngine::getLink('opencast/course/remove_series/'.$course_id.'/'.$serie['identifier'])?>">
-                                    <?= Assets::img('icons/16/blue/trash.png', array('title' => _('Zuordnung löschen'), 'alt' => _('Zuordnung löschen')))?>
-                                </a>
-                            </li>
-                        <? endforeach ?>
+                        <? if (!empty($connectedSeries)) : ?>
+                            <? foreach ($connectedSeries as $serie) : ?>
+                                <li>
+                                    <?= $serie['title'] ?>
+                                    <a href="<?= PluginEngine::getLink('opencast/course/remove_series/' . $course_id . '/' . $serie['identifier']) ?>">
+                                        <?= Assets::img('icons/16/blue/trash.png', array('title' => _('Zuordnung löschen'), 'alt' => _('Zuordnung löschen'))) ?>
+                                    </a>
+                                </li>
+                            <? endforeach ?>
                         <? else : ?>
                             <li style="padding-top:5px">
-                                <p><?=_("Bislang wurde noch keine Series zugeordnet.")?></p>
+                                <p><?= _("Bislang wurde noch keine Series zugeordnet.") ?></p>
                             </li>
                         <? endif ?>
                     </ul>
                 </div>
                 <div style="float:center;text-align:center">
-                    <p> <?//=_("Series ohne Zuordnung")?></p>
-                    <? if(!empty ($notConnectedSeries) || true) :?>
+                    <p> <? //=_("Series ohne Zuordnung") ?></p>
+                    <? if (!empty($notConnectedSeries) || true) : ?>
 
-            
-                    <select multiple="multiple" name="series[]">
-                        <? foreach($notConnectedSeries as $serie) :?>
-                            <? if($s->series->additionalMetadata !=null) : ?>
-                                <option value="<?= $serie['identifier'] ?>">
-                                    <?= $serie['title'] ?>
-                                </option>
-                            <? endif ?>
-                        <? endforeach ?>
-                    </select>
+
+                        <select multiple="multiple" name="series[]">
+                            <? foreach ($notConnectedSeries as $serie) : ?>
+                                <? if ($s->series->additionalMetadata != null) : ?>
+                                    <option value="<?= $serie['identifier'] ?>">
+                                        <?= $serie['title'] ?>
+                                    </option>
+                                <? endif ?>
+                            <? endforeach ?>
+                        </select>
                     <? endif ?>
                 </div>
             </div> 
             <div style="padding-top:2em;clear:both" class="form_submit">
-                <?= makebutton("uebernehmen","input") ?>
-                <a href="<?=PluginEngine::getLink('opencast/course/index')?>"><?= makebutton("abbrechen")?></a>
+                <?= makebutton("uebernehmen", "input") ?>
+                <a href="<?= PluginEngine::getLink('opencast/course/index') ?>"><?= makebutton("abbrechen") ?></a>
             </div>
         </form>
-        <? endif;?>
-    </div>
+    <? endif; ?>
+</div>
--- views/course/config.php
+++ views/course/config.php
@@ -1,39 +1,39 @@
 <? if (isset($message)): ?>
-  <?= MessageBox::success($message) ?>
+    <?= MessageBox::success($message) ?>
 <? endif ?>
 
 <?
 $infobox_content = array(array(
-    'kategorie' => _('Hinweise:'),
-    'eintrag'   => array(array(
-        'icon' => 'icons/16/black/info.png',
-        'text' => _("Hier können Sie die Veranstaltung mit einer Series in Opencast Matterhorn verknüpfen. Sie können entweder aus vorhandenen Series wählen oder eine Series für diese Veranstaltung anlegen.")
-    ))
-));
+        'kategorie' => _('Hinweise:'),
+        'eintrag' => array(array(
+                'icon' => 'icons/16/black/info.png',
+                'text' => _("Hier können Sie die Veranstaltung mit einer Series in Opencast Matterhorn verknüpfen. Sie können entweder aus vorhandenen Series wählen oder eine Series für diese Veranstaltung anlegen.")
+        ))
+        ));
 
 $infobox = array('picture' => 'infobox/administration.jpg', 'content' => $infobox_content);
 ?>
 <script language="JavaScript">
-OC.initAdmin();
+    OC.initAdmin();
 </script>
-<h3><?=_('Verwaltung der eingebundenen Vorlesungsaufzeichnungen')?></h3>
+<h3><?= _('Verwaltung der eingebundenen Vorlesungsaufzeichnungen') ?></h3>
 
-<?if (true || !empty($this->connectedSeries)) : ?>
+<? if (true || !empty($this->connectedSeries)) : ?>
     <?= MessageBox::info(sprintf(_("Sie haben noch keine Series aus Opencast mit dieser Veranstaltung verknüpft.
-                            Bitte verknüpfen eine bereits vorhandene Series oder %s erstellen Sie eine neue.%s"), 
-                         '<a href="'.PluginEngine::getLink('opencast/course/create_series/') . '">', '</a>')) ?>
+                            Bitte verknüpfen eine bereits vorhandene Series oder %s erstellen Sie eine neue.%s"), '<a href="' . PluginEngine::getLink('opencast/course/create_series/') . '">', '</a>'))
+    ?>
     <div id="admin-accordion">
-        <h3><?=_('Wählen Sie unten eine Series aus, die Sie mit der aktuellen Veranstaltung verknüpfen möchten')?>:</h3>
+        <h3><?= _('Wählen Sie unten eine Series aus, die Sie mit der aktuellen Veranstaltung verknüpfen möchten') ?>:</h3>
         <?= $this->render_partial("course/_connectedSeries", array('course_id' => $course_id, 'connectedSeries' => $connectedSeries, 'unonnectedSeries' => $unonnectedSeries, 'series_client' => $series_client)) ?>
     </div>
 
 
-<? elseif(!$connected) : ?>
+<? elseif (!$connected) : ?>
 
 
-   
+
     <div class="oc_schedule">
-<!--    <h3><?=_('Veranstaltungsaufzeichnungen Planen')?>:</h3>
+    <!--    <h3><?= _('Veranstaltungsaufzeichnungen Planen') ?>:</h3>
         <h4>Ablaufplan</h4>
         <p>Erst feststellen ob ein Raum mit CA da ist und dann den Ablaufplan posten mit Checkboxes zum Schedulen...</p> -->
         <table class="default">
@@ -42,57 +42,56 @@ OC.initAdmin();
                 <th>Titel</th>
                 <th>Aktionen</th>
             </tr>
-            
-            <? foreach($dates as $d) : ?>
+
+            <? foreach ($dates as $d) : ?>
                 <tr>
-                <? $date = new SingleDate($d['termin_id']); ?>
-                <td> <?=$date->getDatesHTML()?> </td>
-                <? $issues = $date->getIssueIDs(); ?>
-                <? if(is_array($issues)) : ?>
-                    <? foreach($issues as $is) : ?>
-                        <? $issue = new Issue(array('issue_id' => $is));?>
-                        <? $topic = true; ?>
-                        <td> <?= my_substr($issue->getTitle(), 0 ,80 ); ?></td>
-                    <? endforeach; ?>
-                <? else: ?>
+                    <? $date = new SingleDate($d['termin_id']); ?>
+                    <td> <?= $date->getDatesHTML() ?> </td>
+                    <? $issues = $date->getIssueIDs(); ?>
+                    <? if (is_array($issues)) : ?>
+                        <? foreach ($issues as $is) : ?>
+                            <? $issue = new Issue(array('issue_id' => $is)); ?>
+                            <? $topic = true; ?>
+                            <td> <?= my_substr($issue->getTitle(), 0, 80); ?></td>
+                        <? endforeach; ?>
+                    <? else: ?>
                         <? $topic = false; ?>
-                        <td> <?=_("Kein Titel eingetragen")?></td>
-                <? endif; ?>
-                <td>
-                    <? $resource = $date->getResourceID(); ?>
-                    <? if(isset($resource) && OCModel::checkResource($resource)) :?>
-                        <? if(OCModel::checkScheduled($course_id, $resource, $date->termin_id)) :?>
-                            <a href="<?=PluginEngine::getLink('opencast/course/unschedule/'.$resource .'/'. $date->termin_id ) ?>">
-                                <?= Assets::img('icons/16/blue/video.png', array('title' => _("Aufzeichnung ist bereits geplant. Klicken Sie hier um die Planung zu aufzuheben"))) ?>
-                            </a>
-                        <?  else : ?>
-                            <? if($topic) :?>
-                            <a href="<?=PluginEngine::getLink('opencast/course/schedule/'.$resource .'/'. $date->termin_id ) ?>">
-                                <?= Assets::img('icons/16/blue/date.png', array('title' => _("Aufzeichnung planen"))) ?>
-                            </a>
-                            <? else :?>
-                                <?= Assets::img('icons/16/blue/exclaim-circle.png', array('title' =>  _("Bitte geben Sie ein Thema für diese Veranstaltung an."))) ?>
-                            <? endif ;?>
-                        <? endif; ?>
-                    <?
-                        
-                    ?>
-                        <?// $date->getRoom() ?>
-                    <? elseif(false) : ?>
-                        <?= Assets::img('icons/16/blue/video.png') ?>
-                    <?
-                        /*  Wenn es eine Aufzeichnung gibt, optionen zum Unsichtbar machen anbieten
-                         *  Wenn keine Aufzeichnung aus OC gibt dann ersma nix machen
-                         *
-                         *
-                         */
-                    ?>
-                    <? else : ?>
-                        <?= Assets::img('icons/16/red/exclaim-circle.png', array('title' =>  _("Es wurde bislang kein Raum mit Aufzeichnungstechnik gebucht"))) ?>
+                        <td> <?= _("Kein Titel eingetragen") ?></td>
                     <? endif; ?>
+                    <td>
+                        <? $resource = $date->getResourceID(); ?>
+                        <? if (isset($resource) && OCModel::checkResource($resource)) : ?>
+                            <? if (OCModel::checkScheduled($course_id, $resource, $date->termin_id)) : ?>
+                                <a href="<?= PluginEngine::getLink('opencast/course/unschedule/' . $resource . '/' . $date->termin_id) ?>">
+                                    <?= Assets::img('icons/16/blue/video.png', array('title' => _("Aufzeichnung ist bereits geplant. Klicken Sie hier um die Planung zu aufzuheben"))) ?>
+                                </a>
+                            <? else : ?>
+                                <? if ($topic) : ?>
+                                    <a href="<?= PluginEngine::getLink('opencast/course/schedule/' . $resource . '/' . $date->termin_id) ?>">
+                                        <?= Assets::img('icons/16/blue/date.png', array('title' => _("Aufzeichnung planen"))) ?>
+                                    </a>
+                                <? else : ?>
+                                    <?= Assets::img('icons/16/blue/exclaim-circle.png', array('title' => _("Bitte geben Sie ein Thema für diese Veranstaltung an."))) ?>
+                                <? endif; ?>
+                            <? endif; ?>
+                            <?
+                            ?>
+                            <? // $date->getRoom() ?>
+                        <? elseif (false) : ?>
+                            <?= Assets::img('icons/16/blue/video.png') ?>
+                            <?
+                            /*  Wenn es eine Aufzeichnung gibt, optionen zum Unsichtbar machen anbieten
+                             *  Wenn keine Aufzeichnung aus OC gibt dann ersma nix machen
+                             *
+                             *
+                             */
+                            ?>
+                        <? else : ?>
+                            <?= Assets::img('icons/16/red/exclaim-circle.png', array('title' => _("Es wurde bislang kein Raum mit Aufzeichnungstechnik gebucht"))) ?>
+                        <? endif; ?>
 
 
-                </td>
+                    </td>
                 </tr>
             <? endforeach; ?>
 
@@ -100,36 +99,36 @@ OC.initAdmin();
     </div>
 
 <? else: ?>
-   <table class="default">
-            <tr>
-                <th>Titel</th>
-                <th>Status</th>
-                <th>Aktionen</th>
-            </tr>
-            <? foreach($episodes as $episode) :?>
+    <table class="default">
+        <tr>
+            <th>Titel</th>
+            <th>Status</th>
+            <th>Aktionen</th>
+        </tr>
+        <? foreach ($episodes as $episode) : ?>
 
-                <? if(isset($episode->mediapackage)) :?>
+            <? if (isset($episode->mediapackage)) : ?>
                 <tr>
                     <td>
-                        <?=$episode->mediapackage->title ?>
+                        <?= $episode->mediapackage->title ?>
                     </td>
                     <td>
-                        <?$visible = OCModel::getVisibilityForEpisode($course_id, $episode->mediapackage->id); ?>
-                        <? if($visible['visible'] == 'true') : ?>
-                            <?=_("Sichtbar")?>
+                        <? $visible = OCModel::getVisibilityForEpisode($course_id, $episode->mediapackage->id); ?>
+                        <? if ($visible['visible'] == 'true') : ?>
+                            <?= _("Sichtbar") ?>
                         <? else : ?>
-                            <?=_("Unsichtbar")?>
+                            <?= _("Unsichtbar") ?>
                         <? endif; ?>
                     </td>
-                    
+
                     <td>
-                        <a href="<?=PluginEngine::getLink('opencast/course/toggle_visibility/'.$episode->mediapackage->id ) ?>">
+                        <a href="<?= PluginEngine::getLink('opencast/course/toggle_visibility/' . $episode->mediapackage->id) ?>">
                             <?= Assets::img('icons/16/blue/visibility-visible.png', array('title' => _("Aufzeichnung unsichtbar schalten"))) ?>
                         </a>
                     </td>
                 </tr>
-                <? endif ;?>
-            <? endforeach; ?>
+            <? endif; ?>
+        <? endforeach; ?>
 
-   </table>
+    </table>
 <? endif; ?>
