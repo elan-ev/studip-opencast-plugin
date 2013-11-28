545b41e6b9791a6e6fdd0e4867e0197ef57b05c3
aklassen <andre.klassen@gmail.com>
2013-11-28 16:07:37 +0100

    refine resources_objects_properties  to avoid errors while reinstalling the plugin

--- models/OCModel.php
+++ models/OCModel.php
@@ -39,7 +39,7 @@ class OCModel
     static function getOCRessources() {
        $stmt = DBManager::get()->prepare("SELECT * FROM resources_objects ro
                 LEFT JOIN resources_objects_properties rop ON (ro.resource_id = rop.resource_id)
-                WHERE rop.property_id = (SELECT property_id FROM resources_properties WHERE name = 'Opencast Capture Agent' )
+                WHERE rop.property_id IN (SELECT property_id FROM resources_properties WHERE name = 'Opencast Capture Agent' )
                 AND rop.state = 'on'");
 
        $stmt->execute();
