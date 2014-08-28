a91cb7e57a03ee850552773a314e55cb22bfbc4c
André Klaßen <aklassen@uos.de>
2014-08-28 17:04:48 +0200

    refine course config fixes #13

---
+++ views/course/_config.php
@@ -0,0 +1,29 @@
+<? use Studip\Button, Studip\LinkButton; ?>
+<div>
+    <span class="oc_config_infotext">
+        <?=_('Wählen Sie eine Series aus, die Sie mit der aktuellen Veranstaltung verknüpfen möchten.')?>
+    </span>
+    <form action="<?= PluginEngine::getLink('opencast/course/edit/' . $course_id) ?>"
+      method=post id="select-series" data-unconnected="<?= (empty($connectedSeries) ? 1 : 'false');?>">
+        <div style="text-align: center;">
+            <? if (!empty($unconnectedSeries)) : ?>
+                <select class="series_select" multiple="multiple" name="series[]">
+
+                    <? foreach ($unconnectedSeries as $serie) : ?>
+                        <? if (isset($serie['identifier'])) : ?>
+
+                            <option value="<?= $serie['identifier'] ?>">
+                                <?= utf8_decode($serie['title'])?>
+                                <? //= $series->additionalMetadata?>
+                            </option>
+                        <? endif ?>
+                    <? endforeach ?>
+                </select>
+            <? endif ?>
+        </div>
+        <div style="padding-top:2em;clear:both" class="form_submit change_series">
+            <?= Button::createAccept(_('Übernehmen'), array('title' => _("Änderungen übernehmen"))); ?>
+            <?= LinkButton::createCancel(_('Abbrechen'), PluginEngine::getLink('opencast/course/index')); ?>
+        </div>
+    </form>
+</div>
