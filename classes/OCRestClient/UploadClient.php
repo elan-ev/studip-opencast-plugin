ee13acf4c030ec4b0ca6836dc7aaec72ec19f379
Julian Jacobi <julian.jacobi@tu-braunschweig.de>
2017-03-22 10:52:09 +0100

    change newJob REST access from custom curl to general REST class

--- classes/OCRestClient/UploadClient.php
+++ classes/OCRestClient/UploadClient.php
@@ -28,26 +28,15 @@ class UploadClient extends OCRestClient {
      */
     function newJob($name, $size, $chunksize, $flavor, $mediaPackage) {
         $data = array(
-            'filename' => urlencode($name),
+            'filename' => $name,
             'filesize' => $size,
             'chunksize' =>  $chunksize,
-            'flavor' => urlencode($flavor),
-            'mediapackage' => urlencode($mediaPackage)
+            'flavor' => $flavor,
+            'mediapackage' => $mediaPackage
         );
         $rest_end_point = "/newjob";
-        $uri = $rest_end_point;
-        
-        // setting up a curl-handler
-        curl_setopt($this->ochandler,CURLOPT_URL,$this->matterhorn_base_url.$uri);
-        curl_setopt($this->ochandler, CURLOPT_POST, true);
-        curl_setopt($this->ochandler, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($this->ochandler, CURLOPT_HEADER, false); // we don't need that kind of information now
-        //TODO über REST Klasse laufen lassen, getXML, getJSON...
 
-        $response = curl_exec($this->ochandler);
-        $httpCode = curl_getinfo($this->ochandler, CURLINFO_HTTP_CODE);
-        if ($httpCode == 200 && isset($response)){
-            error_log($response);
+        if($response = $this->getXML($rest_end_point, $data, false)) {
             return $response;
         } else {
             return false;
