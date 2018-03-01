7ef0edbddecda0f545fc2e4a5084614693ea344b
jleissner <jleissner@uos.de>
2018-03-01 15:14:24 +0100

    added requirements for cronjob

--- cronjobs/try_reupload_failed_jobs.php
+++ cronjobs/try_reupload_failed_jobs.php
@@ -7,6 +7,22 @@
 
 require_once 'lib/classes/CronJob.class.php';
 
+require_once $this->trails_root . '/classes/OCRestClient/SearchClient.php';
+require_once $this->trails_root . '/classes/OCRestClient/SeriesClient.php';
+require_once $this->trails_root . '/classes/OCRestClient/IngestClient.php';
+require_once $this->trails_root . '/classes/OCRestClient/UploadClient.php';
+require_once $this->trails_root . '/classes/OCRestClient/ArchiveClient.php';
+require_once $this->trails_root . '/models/OCModel.php';
+require_once $this->trails_root . '/models/OCSeriesModel.php';
+require_once $this->trails_root . '/models/OCCourseModel.class.php';
+require_once $this->trails_root . '/models/OCEndpointModel.php';
+
+require_once $this->trails_root . '/classes/OCJsonFile.php';
+
+require_once $this->trails_root . '/classes/OCJobManager.php';
+require_once $this->trails_root . '/classes/OCJob.php';
+require_once $this->trails_root . '/classes/OCJobLocation.php';
+
 
 class TryReuploadFailedJobs extends CronJob {
 
