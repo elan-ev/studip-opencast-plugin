e931d2e54d7eb66916d8f57f57813f1fa6873ca8
tgloeggl <tgloeggl@uos.de>
2018-03-02 13:45:28 +0100

    fix table creations

--- migrations/001_init_ocplugin.php
+++ migrations/001_init_ocplugin.php
@@ -2,14 +2,14 @@
 
 class InitOcplugin extends Migration {
     function up() {
-        
-                   
+
+
         DBManager::get()->query("CREATE TABLE IF NOT EXISTS `oc_config` (
               `service_url` varchar(255) NOT NULL,
               `service_user` varchar(255) NOT NULL,
               `service_password` varchar(255) NOT NULL,
               PRIMARY KEY (`service_url`)
-              ) ENGINE=MyISAM;");
+              )  ROW_FORMAT=DYNAMIC;");
 
         DBManager::get()->query("CREATE TABLE IF NOT EXISTS `oc_seminar_series` (
                 `seminar_id` VARCHAR( 32 ) NOT NULL ,
@@ -17,25 +17,25 @@ class InitOcplugin extends Migration {
                 `visibility` ENUM(  'visible',  'invisible' )NOT NULL ,
                 `schedule` TINYINT( 1 ) NOT NULL DEFAULT '0',
                 PRIMARY KEY (  `seminar_id` ,  `series_id` )
-                ) ENGINE = MYISAM;");
-        
+                );");
+
         DBManager::get()->query("CREATE TABLE IF NOT EXISTS `oc_resources` (
                 `resource_id` VARCHAR( 32 ) NOT NULL ,
                 `capture_agent` VARCHAR( 64 ) NOT NULL ,
                 PRIMARY KEY (  `resource_id` ,  `capture_agent` )
-                ) ENGINE = MYISAM;");
+                );");
 
         DBManager::get()->query("INSERT INTO `resources_properties`
                 (`property_id`, `name`, `description`, `type`, `options`, `system`)
                 VALUES (MD5('".uniqid()."'), 'Opencast Capture Agent', '', 'bool', 'vorhanden', 0)");
 
-        
+
         DBManager::get()->query("CREATE TABLE IF NOT EXISTS `oc_seminar_episodes` (
                 `seminar_id` VARCHAR( 32 ) NOT NULL ,
                 `episode_id` VARCHAR( 64 ) NOT NULL ,
                 `visible` ENUM( 'true', 'false' ) NOT NULL DEFAULT 'true',
                 PRIMARY KEY ( `seminar_id` , `episode_id` )
-                ) ENGINE = MYISAM;");
+                );");
 
 
         DBManager::get()->query("CREATE TABLE IF NOT EXISTS `oc_scheduled_recordings` (
@@ -47,7 +47,7 @@ class InitOcplugin extends Migration {
                 `event_id` VARCHAR( 64 ) NOT NULL,
                 `status` ENUM( 'scheduled', 'recorded' ) NOT NULL ,
                 PRIMARY KEY ( `seminar_id` , `series_id` , `date_id` , `resource_id` , `capture_agent` )
-                ) ENGINE = MYISAM;");
+                );");
 
 
                  DBManager::get()->query("CREATE TABLE IF NOT EXISTS `oc_endpoints` (
@@ -55,18 +55,18 @@ class InitOcplugin extends Migration {
                    `service_host` varchar(255) NOT NULL DEFAULT '',
                    `service_type` varchar(255) NOT NULL DEFAULT '',
                    PRIMARY KEY (`service_url`)
-                 ) ENGINE=MyISAM;");
+                 )  ROW_FORMAT=DYNAMIC;");
 
 
 
 
     }
-    
+
     function down() {
         DBManager::get()->query("DROP TABLE IF EXISTS `oc_series`,`oc_config`,`oc_endpoints`, `oc_seminar_series`, `oc_resources`, `oc_seminar_episodes`, `oc_scheduled_recordings`;");
-        DBManager::get()->query("DELETE FROM  resources_objects_properties 
+        DBManager::get()->query("DELETE FROM  resources_objects_properties
         WHERE property_id IN(SELECT property_id FROM resources_properties WHERE name = 'Opencast Capture Agent' );");
         DBManager::get()->query("DELETE FROM resources_properties WHERE name = 'Opencast Capture Agent';");
     }
-    
+
 }
--- migrations/002_workflow_storage.php
+++ migrations/002_workflow_storage.php
@@ -3,17 +3,17 @@
 class WorkflowStorage extends Migration {
     function up() {
         //seminar_id, workflow_id, user_id
-                   
+
         DBManager::get()->query("CREATE TABLE IF NOT EXISTS `oc_seminar_workflows` (
               `workflow_id` varchar(255) NOT NULL,
               `seminar_id` varchar(32) NOT NULL,
               `user_id` varchar(32) NOT NULL,
               PRIMARY KEY (`workflow_id`)
-              ) ENGINE=MyISAM;");
+              ) ROW_FORMAT=DYNAMIC;");
     }
-    
+
     function down() {
         DBManager::get()->query("DROP TABLE IF EXISTS `oc_seminar_workflows`;");
     }
-    
+
 }
