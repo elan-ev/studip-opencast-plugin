a534a85fc9e383b751554f7721350cc054509e05
tgloeggl <tgloeggl@uos.de>
2018-03-26 13:24:11 +0200

    fix endpoint listing

--- views/admin/_endpointoverview.php
+++ views/admin/_endpointoverview.php
@@ -2,17 +2,21 @@
 <form class="conf-form" action="<?= PluginEngine::getLink('opencast/admin/update_endpoints/') ?>" method=post>
     <?= CSRFProtection::tokenTag() ?>
 
-        <table border="0" cellspacing="5" cellpadding="5" class="default">
-            <tr><th><?=$_("Endpoint")?></th><th><?=$_("Host")?></th><th><?=$_("Status")?></th></tr>
-            <? foreach($endpoints as $endpoint) :?>
-            <tr><td><?=$endpoint['service_type']?></td><td><?=$endpoint['service_url']?></td><td>
-                <? if (!@$fp = fsockopen($endpoint['service_url'],8080, $errno, $errstr, 1)) : ?>
-                    <?= Assets::img('icons/16/red/exclaim-circle.png', array('title' => $_("Der Endpoint ist nicht erreichbar."))); ?>
-                <? else : ?>
-                   <?= Assets::img('icons/16/green/accept.png', array('title' => $_("Der Endpoint ist erreichbar."))); ?>
-               <? endif; ?>
-            </td></tr>
-            <? endforeach; ?>
-        </table>
+    <table border="0" cellspacing="5" cellpadding="5" class="default">
+        <tr>
+            <th><?= $_("Endpoint") ?></th>
+            <th><?= $_("Host") ?></th>
+        </tr>
 
+        <? foreach($endpoints as $endpoint) :?>
+        <tr>
+            <td>
+                <?= $endpoint['service_type']?>
+            </td>
+            <td>
+                <?=$endpoint['service_url']?>
+            </td>
+        </tr>
+        <? endforeach; ?>
+    </table>
 </form>
