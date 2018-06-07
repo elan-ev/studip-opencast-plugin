4e5d3476049ff8fe6b2389cd523841f7bbb81dd6
jleissner <jleissner@uos.de>
2018-06-07 11:36:42 +0200

    coding conventions

--- views/admin/config.php
+++ views/admin/config.php
@@ -1,18 +1,24 @@
 <?
-    use Studip\Button, Studip\LinkButton;
-    $sidebar = Sidebar::get();
-    $actions = new ActionsWidget();
-    $actions->addLink($_("Episoden aller Series abgleichen"), PluginEngine::getLink ('opencast/admin/refresh_episodes/' . get_ticket()), new Icon('refresh', 'clickable'));
-    $sidebar->addWidget($actions);
+$sidebar = Sidebar::get();
+$actions = new ActionsWidget();
+$actions->addLink($_("Episoden aller Series abgleichen"), PluginEngine::getLink('opencast/admin/refresh_episodes/' . get_ticket()), new Icon('refresh', 'clickable'));
+$sidebar->addWidget($actions);
 
 
-    Helpbar::get()->addPlainText('',$_("Hier kann die Anbindung zum Opencast System verwaltet werden."));
+Helpbar::get()->addPlainText('', $_("Hier kann die Anbindung zum Opencast System verwaltet werden."));
 ?>
 
 <?= $this->render_partial('messages') ?>
 
 <script language="JavaScript">
-OC.initAdmin();
+    OC.initAdmin();
 </script>
-<?= $this->render_partial("admin/_initial_config", array('info_url' => $info_url,
-                              'info_user' =>$info_user,'info_password' => $info_password)) ?>
+
+<?= $this->render_partial(
+    "admin/_initial_config",
+    [
+        'info_url'      => $info_url,
+        'info_user'     => $info_user,
+        'info_password' => $info_password
+    ]
+) ?>
