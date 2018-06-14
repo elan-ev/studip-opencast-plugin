c0f73c217ee996cceb8cd4214d21d83e4e4b4caf
jleissner <jleissner@uos.de>
2018-06-14 14:41:13 +0200

    coding conventions

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,18 +1,18 @@
 OC = {
-    formData : {},
-    initAdmin : function(){
-        jQuery(document).ready(function(){
+    formData: {},
+    initAdmin: function () {
+        jQuery(document).ready(function () {
             jQuery('#admin-accordion').accordion();
         });
     },
-    initIndexpage: function(){
-        jQuery( document ).ready(function() {
+    initIndexpage: function () {
+        jQuery(document).ready(function () {
 
             var cid = jQuery('#course_id').data('courseid');
 
-            if(STUDIP.hasperm) {
-                 // Upload Dialog
-                jQuery("#upload_dialog").dialog({ autoOpen: false, width: 800, dialogClass: 'ocUpload'});
+            if (STUDIP.hasperm) {
+                // Upload Dialog
+                jQuery("#upload_dialog").dialog({autoOpen: false, width: 800, dialogClass: 'ocUpload'});
                 jQuery("#oc_upload_dialog").click(
                     function () {
                         jQuery("#upload_dialog").dialog('open');
@@ -20,7 +20,7 @@ OC = {
                     }
                 );
                 // Config Dialog
-                jQuery("#config_dialog").dialog({ autoOpen: false, width: 800, dialogClass: 'ocConfig', height: 350});
+                jQuery("#config_dialog").dialog({autoOpen: false, width: 800, dialogClass: 'ocConfig', height: 350});
                 jQuery("#oc_config_dialog").click(
                     function () {
                         jQuery("#config_dialog").dialog('open');
@@ -28,12 +28,12 @@ OC = {
                     }
                 );
                 // Download Dialog
-                jQuery('[id^="download_dialog"]').each(function(index,element){
-                    jQuery(element).dialog({ autoOpen: false, width: 800, dialogClass: 'ocDownload'});
+                jQuery('[id^="download_dialog"]').each(function (index, element) {
+                    jQuery(element).dialog({autoOpen: false, width: 800, dialogClass: 'ocDownload'});
                 });
-                jQuery(".oc_download_dialog").each(function(index,element){
-                    jQuery(element).click(function(){
-                        jQuery("#download_dialog-"+jQuery(element).data('episode_id')).dialog('open');
+                jQuery(".oc_download_dialog").each(function (index, element) {
+                    jQuery(element).click(function () {
+                        jQuery("#download_dialog-" + jQuery(element).data('episode_id')).dialog('open');
                         return false;
                     });
                 });
@@ -52,7 +52,7 @@ OC = {
             }
 
 
-            if(OC.states && STUDIP.hasperm){
+            if (OC.states && STUDIP.hasperm) {
                 OC.getWorkflowProgressForCourse(cid, true, null);
             }
 
@@ -62,9 +62,9 @@ OC = {
 
     },
 
-    initUpload : function(maxChunk){
-        jQuery(document).ready(function(){
-            $('#btn_accept').click(function() {
+    initUpload: function (maxChunk) {
+        jQuery(document).ready(function () {
+            $('#btn_accept').click(function () {
                 OC.formData.submit();
                 return false;
             });
@@ -73,15 +73,15 @@ OC = {
                 limitMultiFileUploads: 1,
                 autoupload: false,
                 maxChunkSize: maxChunk,
-                add: function(e, data) {
+                add: function (e, data) {
                     var file = data.files[0];
                     $('#total_file_size').attr('value', file.size);
                     $('#file_name').attr('value', file.name);
                     $('#upload_info').html('<p>Name: '
-                                                + file.name
-                                                + ' Gr&ouml;&szlig;e: '
-                                                + OC.getFileSize(file.size)
-                                                + '</p>');
+                        + file.name
+                        + ' Gr&ouml;&szlig;e: '
+                        + OC.getFileSize(file.size)
+                        + '</p>');
                     $('#upload_info').val(file.name);
                     OC.formData = data;
                     return false;
@@ -92,7 +92,7 @@ OC = {
                         value: 0
                     }).addClass('oc_mediaupload_progressbar').show().css({'background': '#d0d7e4'});
                 },
-                progressall: function(e, data) {
+                progressall: function (e, data) {
                     if (data.bitrate / 8 > 1048576) {
                         var speed = parseInt(data.bitrate / 8 / 1024 / 1024) + " Mb/s";
                     } else {
@@ -100,15 +100,15 @@ OC = {
                     }
 
                     var progress = parseInt(data.loaded / data.total * 100, 10);
-                    jQuery( "#progressbar" ).progressbar( "value", progress);
+                    jQuery("#progressbar").progressbar("value", progress);
                     jQuery("#progressbar-label").text(progress + " % / " + speed);
                 },
-                done: function(e, data) {
-                    jQuery( "#progressbar" ).progressbar('destroy');
+                done: function (e, data) {
+                    jQuery("#progressbar").progressbar('destroy');
                     jQuery("#upload_dialog").dialog("close");
                     window.open(STUDIP.URLHelper.getURL("plugins.php/opencast/course/index/false/true"), '_self');
                 },
-                error: function(xhr, data) {
+                error: function (xhr, data) {
                     console.log('Fehler', data);
                 }
             });
@@ -117,13 +117,13 @@ OC = {
             });
         })
     },
-    getFileSize: function(input) {
-        if(input/1024 > 1) {
-            var inp_kb = Math.round((input/1024)*100)/100
-            if(inp_kb/1024 > 1) {
-                var inp_mb = Math.round((inp_kb/1024)*100)/100
-                if(inp_mb/1024 > 1) {
-                    var inp_gb = Math.round((inp_mb/1024)*100)/100
+    getFileSize: function (input) {
+        if (input / 1024 > 1) {
+            var inp_kb = Math.round((input / 1024) * 100) / 100
+            if (inp_kb / 1024 > 1) {
+                var inp_mb = Math.round((inp_kb / 1024) * 100) / 100
+                if (inp_mb / 1024 > 1) {
+                    var inp_gb = Math.round((inp_mb / 1024) * 100) / 100
                     return inp_gb + 'GB';
                 }
                 return inp_mb + 'MB';
@@ -133,14 +133,14 @@ OC = {
         return input + 'Bytes'
     },
 
-    getWorkflowProgressForCourse: function(course_id, animation, info) {
+    getWorkflowProgressForCourse: function (course_id, animation, info) {
 
         var reload = false;
-        if(info == null){
+        if (info == null) {
             info = [];
-            info[0] = [0,0,0];
+            info[0] = [0, 0, 0];
         }
-        jQuery.get(STUDIP.ABSOLUTE_URI_STUDIP + "plugins.php/opencast/ajax/getWorkflowStatusforCourse/" +  course_id).done(function(data) {
+        jQuery.get(STUDIP.ABSOLUTE_URI_STUDIP + "plugins.php/opencast/ajax/getWorkflowStatusforCourse/" + course_id).done(function (data) {
             var response = jQuery.parseJSON(data);
 
             if (!jQuery.isEmptyObject(response)) {
@@ -148,7 +148,7 @@ OC = {
 
                     var job = response[job_id];
 
-                    if (job.state == 'RUNNING' || job.state == 'INSTANTIATED' ) {
+                    if (job.state == 'RUNNING' || job.state == 'INSTANTIATED') {
 
                         var counter = 1;
                         var current_description = "";
@@ -158,38 +158,41 @@ OC = {
 
                             if (job.operations.operation[operation].state == 'RUNNING'
                                 || job.operations.operation[operation].state == 'INSTANTIATED'
-                            ){
+                            ) {
                                 current_description = job.operations.operation[operation].description;
                                 break;
                             }
                         }
 
                         info[info.length] = [
-                            (counter>job.operations.operation.length?job.operations.operation.length:counter),
+                            (counter > job.operations.operation.length ? job.operations.operation.length : counter),
                             job.operations.operation.length,
-                            counter/job.operations.operation.length
+                            counter / job.operations.operation.length
                         ];
 
-                        jQuery('#'+job_id).attr('title',counter +'/'+ job.operations.operation.length +': '+ current_description);
-                        jQuery('#'+job_id).attr('alt', counter +'/'+ job.operations.operation.length +': '+ current_description);
+                        jQuery('#' + job_id).attr('title', counter + '/' + job.operations.operation.length + ': ' + current_description);
+                        jQuery('#' + job_id).attr('alt', counter + '/' + job.operations.operation.length + ': ' + current_description);
                     }
-                    else if(jQuery('#'+job_id).length > 0){
+                    else if (jQuery('#' + job_id).length > 0) {
                         reload = true;
                     }
                 }
 
-            } if((reload || response == "")){
+            }
+            if ((reload || response == "")) {
                 window.open(STUDIP.ABSOLUTE_URI_STUDIP + "plugins.php/opencast/course/index/false", '_self');
             } else {
-                window.setTimeout(function(){OC.getWorkflowProgressForCourse(course_id, false, info)}, 5000)
+                window.setTimeout(function () {
+                    OC.getWorkflowProgressForCourse(course_id, false, info)
+                }, 5000)
             }
 
         });
 
     },
 
-    toggleVis: function(cid){
-        jQuery('#oc-togglevis').click(function(e) {
+    toggleVis: function (cid) {
+        jQuery('#oc-togglevis').click(function (e) {
             e.preventDefault();
             var episode_id = jQuery('#oc-togglevis').data("episode-id");
 
@@ -207,9 +210,9 @@ OC = {
 
     },
 
-    episodeListener: function(cid) {
+    episodeListener: function (cid) {
         // open episode item
-        jQuery('.oce_item').click(function(e){
+        jQuery('.oce_item').click(function (e) {
             e.preventDefault();
 
             var episode_id = jQuery(this).attr('id');
@@ -222,17 +225,17 @@ OC = {
             jQuery('#oc_balls').show();
             jQuery('.oce_playercontainer').addClass('oc_opaque');
 
-            jQuery.get(STUDIP.ABSOLUTE_URI_STUDIP + "plugins.php/opencast/course/get_player/" +  episode_id + "/" +  cid).done(function(data) {
+            jQuery.get(STUDIP.ABSOLUTE_URI_STUDIP + "plugins.php/opencast/course/get_player/" + episode_id + "/" + cid).done(function (data) {
 
                 var episode = data.episode_data;
                 var dozent = data.perm;
                 var player_template = _.template(jQuery('#playerTemplate').html());
                 var player_template_data = {
-                    episode:episode,
-                    theodul:data.theodul,
-                    dozent:dozent,
-                    engage_player_url:data.engage_player_url,
-                    video:data.video
+                    episode: episode,
+                    theodul: data.theodul,
+                    dozent: dozent,
+                    engage_player_url: data.engage_player_url,
+                    video: data.video
                 };
 
                 jQuery('.oce_playercontainer').empty();
@@ -247,29 +250,29 @@ OC = {
         });
     },
 
-    searchEpisodeList: function(){
+    searchEpisodeList: function () {
         var options = {
-            valueNames: [ 'oce_list_title', 'oce_list_date' ]
+            valueNames: ['oce_list_title', 'oce_list_date']
         };
 
         var episodeList = new List('episodes', options);
     },
 
     // schedule setting
-    initScheduler: function() {
-        $('.wfselect').change(function(){
+    initScheduler: function () {
+        $('.wfselect').change(function () {
 
 
             var workflow_id = $("option:selected", this).attr('value');
-            var termin_id   = $("option:selected", this).data('terminid');
+            var termin_id = $("option:selected", this).data('terminid');
             var resource_id = $("option:selected", this).data('resource');
 
 
-            jQuery.get(STUDIP.ABSOLUTE_URI_STUDIP + "plugins.php/opencast/course/setworkflowforscheduledepisode/" +  termin_id + "/" + workflow_id + "/" + resource_id).done(function(data) {
-                if(!jQuery.isEmptyObject(data)){
+            jQuery.get(STUDIP.ABSOLUTE_URI_STUDIP + "plugins.php/opencast/course/setworkflowforscheduledepisode/" + termin_id + "/" + workflow_id + "/" + resource_id).done(function (data) {
+                if (!jQuery.isEmptyObject(data)) {
                     console.log(data);
                     //todo trigger success message
-                    if(data === 'true'){
+                    if (data === 'true') {
                         //console.log('lööpt'); TODO STUD.IP Success Box triggern
                     } else {
                         alert('Der Workflow konnte für die geplante Aufzeichnung nicht gesetzt werden.')
@@ -280,5 +283,5 @@ OC = {
 
 
         })
-    },
+    }
 };
