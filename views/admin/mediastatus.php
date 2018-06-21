b351a05a1fd58f2d0cf77ceb13e5855d8cb6a372
jleissner <jleissner@uos.de>
2018-06-21 15:01:15 +0200

    nice view

--- views/admin/mediastatus.php
+++ views/admin/mediastatus.php
@@ -1,25 +1,23 @@
 <?php
 /**
- * @author          Jan-Frederik Leissner <jleissner@uos.de>
+ * @author              Jan-Frederik Leissner <jleissner@uos.de>
  * @copyright       (c) Authors
- * @version         1.0 (11:24)
+ * @version             1.0 (11:24)
  */
 ?>
 
-<h3>space on system</h3>
-<pre>
-    <?= var_dump($memory_space); ?>
-    <?= $quota ?>
-</pre>
+<h3>used space on system</h3>
+<?= $memory_space['readable']['used'] ?> of <?= $memory_space['readable']['total'] ?> :
+<progress value="<?= $memory_space['bytes']['used'] ?>" max="<?= $memory_space['bytes']['total'] ?>" data-label="test"></progress>
 
 <h3>opencast successful upload jobs</h3>
-<pre><?= var_dump($upload_jobs['successful'])?></pre>
+<pre><?= var_dump($upload_jobs['successful']) ?></pre>
 
 <h3>uploaded</h3>
-<pre><?= var_dump($uploaded_episodes)?></pre>
+<pre><?= var_dump($uploaded_episodes) ?></pre>
 
 <h3>opencast unfinished upload jobs</h3>
-<pre><?= var_dump($upload_jobs['unfinished'])?></pre>
+<pre><?= var_dump($upload_jobs['unfinished']) ?></pre>
 
 <h3>currently ingesting</h3>
 <pre><?= var_dump($uploading_episodes) ?></pre>
