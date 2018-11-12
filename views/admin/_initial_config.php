269380328099a693b64859463b2d70722eecd272
jleissner <jleissner@uos.de>
2018-11-12 14:58:34 +0100

    moved input field

--- views/admin/_initial_config.php
+++ views/admin/_initial_config.php
@@ -33,6 +33,12 @@
                 placeholder="ENDPOINT_USER_PASSWORD">
         </label>
 
+        <label>
+            <?= $_("Zeitpuffer (in Sekunden)") ?>
+            <?= tooltipIcon($_('Zur Verhinderung von Aufzeichnungsüberlappungen bei Vorausplanung.'))?>
+            <input name="config[<?= $config_id ?>][puffer]" type="number" min="1" step="1" value="<?= $config[$config_id]['schedule_time_puffer_seconds'] ?>">
+        </label>
+
         <? if ($config[$config_id]['service_version']) : ?>
         <label>
             <?= $_('Opencast Basisversion') ?><br>
@@ -40,20 +46,6 @@
         </label>
         <? endif ?>
 
-    </fieldset>
-
-    <fieldset class="conf-form-field">
-        <legend>
-            <? if ($config_id == 1) : ?>
-                <?= $_("Zeitpuffer für Aufzeichnungsplanungen (Aufzeichnung)") ?>
-            <? else : ?>
-                <?=$_("Zeitpuffer für Aufzeichnungsplanungen (Lesezugriff)")?>
-            <? endif ?>
-        </legend>
-        <label>
-            <?= $_("Zeitpuffer (in Sekunden) zur Verhinderung von Aufzeichnungsüberlappungen bei Vorausplanung") ?>
-            <input name="config[<?= $config_id ?>][puffer]" type="number" min="1" step="1" value="<?= $config[$config_id]['schedule_time_puffer_seconds'] ?>">
-        </label>
     </fieldset>
     <? endforeach ?>
 
