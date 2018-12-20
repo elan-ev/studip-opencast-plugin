92fe18bb622cb39a17badece765ecf69fcb83441
jleissner <jleissner@uos.de>
2018-12-20 13:53:59 +0100

    better migration

--- migrations/013_add_precise_config.php
+++ migrations/013_add_precise_config.php
@@ -10,22 +10,24 @@ class AddSchedulePuffer extends Migration
 
     function up()
     {
-        DBManager::get()->query("create table oc_config_precise( id int auto_increment, name varchar(100) null, description text null, value varchar(255) null, for_config int null, constraint oc_config_precise_id_uindex unique (id)); alter table oc_config_precise add primary key (id); ");
-        Configuration::instance(OC_GLOBAL_CONFIG_ID)->set('upload_chunk_size', 10000000, 'Größe der Chunks für den Upload in Byte');
-        Configuration::instance(OC_GLOBAL_CONFIG_ID)->set('number_of_configs', 1, 'Maximale Anzahl von sichtbaren Konfigurationen');
-        Configuration::instance(OC_GLOBAL_CONFIG_ID)->set('time_buffer_overlap', 60, 'Zeitpuffer (in Sekunden) um Überlappungen zu verhindern');
-        Configuration::instance(OC_GLOBAL_CONFIG_ID)->set('upload_encoding', 'UTF-8', 'Encoding für den Upload');
-        Configuration::instance(OC_GLOBAL_CONFIG_ID)->set('ssl_verify_peer', 'false', 'SSL Zertifikat des Peers prüfen');
-        Configuration::instance(OC_GLOBAL_CONFIG_ID)->set('ssl_verify_host', 'false', 'SSL Zertifikat des Hosts prüfen');
-        Configuration::instance(OC_GLOBAL_CONFIG_ID)->set('ssl_cipher_list', 'none', 'Zu benutzende SSL Chiffren');
+        //setup precise config
+        DBManager::get()->query('DROP TABLE IF EXISTS `oc_config_precise`');
+        DBManager::get()->query('CREATE TABLE IF NOT EXISTS `oc_config_precise` (`id` int(11) NOT NULL,`name` varchar(100) COLLATE latin1_german1_ci DEFAULT NULL,`description` text COLLATE latin1_german1_ci,`value` varchar(255) COLLATE latin1_german1_ci DEFAULT NULL,`for_config` int(11) DEFAULT NULL)');
+        DBManager::get()->query("INSERT INTO `oc_config_precise`(`id`, `name`, `description`, `value`, `for_config`) VALUES (1, 'upload_chunk_size', 'Größe der Chunks für den Upload in Byte', '10000000', -1), (2, 'number_of_configs', 'Maximale Anzahl von sichtbaren Konfigurationen', '1', -1), (3, 'time_buffer_overlap', 'Zeitpuffer (in Sekunden) um Überlappungen zu verhindern', '60', -1), (4, 'upload_encoding', 'Encoding für den Upload', 'UTF-8', -1), (5, 'ssl_verify_peer', 'SSL Zertifikat des Peers prüfen', 'false', -1), (6, 'ssl_verify_host', 'SSL Zertifikat des Hosts prüfen', 'false', -1), (7, 'ssl_cipher_list', 'Zu benutzende SSL Chiffren', 'none', -1), (8, 'capture_agent_attribute', 'Name der Capture Agents als Ressourcen-Objekte ', 'Opencast Capture Agent', -1)");
+        DBManager::get()->query('ALTER TABLE `oc_config_precise` ADD UNIQUE KEY `oc_config_precise_id_uindex`(`id`)');
+        DBManager::get()->query('ALTER TABLE `oc_config_precise` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9');
 
+        //remove column from old config
         DBManager::get()->query("ALTER TABLE `oc_config` DROP COLUMN `schedule_time_puffer_seconds`;");
     }
 
     function down()
     {
+        //remove precise config
+        DBManager::get()->query('DROP TABLE oc_config_precise');
+
+        //readd
         DBManager::get()->query("ALTER TABLE `oc_config` ADD `schedule_time_puffer_seconds` int DEFAULT 300 NOT NULL;");
-        DBManager::get()->query("truncate table oc_config_precise;");
     }
 
 }
