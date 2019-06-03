ab9af01eae7c5264c5d19996dbb8be9f455cbf96
tgloeggl <tgloeggl@uos.de>
2019-06-03 18:08:30 +0200

    exclude rtmps as well in mediadownload links

--- models/OCCourseModel.class.php
+++ models/OCCourseModel.class.php
@@ -244,7 +244,7 @@ class OCCourseModel
                 foreach ($tracks as $track) {
                     if ($track->type === 'presenter/delivery') {
                         $parsed_url = parse_url($track->url);
-                        if ($track->mimetype === 'video/mp4' || $track->mimetype === 'video/avi' && (in_array('atom', $track->tags->tag) && $parsed_url['scheme'] != 'rtmp') && !empty($track->video)) {
+                        if ($track->mimetype === 'video/mp4' || $track->mimetype === 'video/avi' && (in_array('atom', $track->tags->tag) && $parsed_url['scheme'] != 'rtmp' && $parsed_url['scheme'] != 'rtmps') && !empty($track->video)) {
                             $quality = $this->calculate_size(
                                 $track->video->bitrate,
                                 $track->duration
@@ -265,9 +265,9 @@ class OCCourseModel
                             ];
                         }
                     }
-                    if (($track->type === 'presentation/delivery') && ($track->mimetype === 'video/mp4' || $track->mimetype === 'video/avi' && (in_array('atom', $track->tags->tag) && $parsed_url['scheme'] != 'rtmp') && !empty($track->video))) {
+                    if (($track->type === 'presentation/delivery') && ($track->mimetype === 'video/mp4' || $track->mimetype === 'video/avi' && (in_array('atom', $track->tags->tag) && $parsed_url['scheme'] != 'rtmp' && $parsed_url['scheme'] != 'rtmps') && !empty($track->video))) {
                         $url = parse_url($track->url);
-                        if (in_array('atom', $track->tags->tag) && $url['scheme'] != 'rtmp') {
+                        if (in_array('atom', $track->tags->tag) && $url['scheme'] != 'rtmp' && && $url['scheme'] != 'rtmps') {
                             $quality = $this->calculate_size(
                                 $track->video->bitrate,
                                 $track->duration
