b601fd3bc8b0c91790ab2d116162c5f331d95dc4
Till Glöggler <till@gundk.it>
2020-03-16 18:33:02 +0100

    catch pdo errors if primary key does not exist

--- migrations/020_add_key_for_accesscontrol.php
+++ migrations/020_add_key_for_accesscontrol.php
@@ -10,8 +10,11 @@ class AddKeyForAccesscontrol extends Migration
     public function up()
     {
         $db = DBManager::get();
-        $db->exec("ALTER TABLE `oc_access_control`
-            DROP INDEX IF EXISTS `PRIMARY`");
+
+        try {
+            $db->exec("ALTER TABLE `oc_access_control`
+                DROP INDEX `PRIMARY`");
+        } catch (PDOException $e) {}
 
         $db->exec("ALTER TABLE `oc_access_control`
             ADD PRIMARY KEY `id_course_id` (`id`(64), `course_id`(32))");
