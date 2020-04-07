4e50623c2cba4c960f45f8278e771bffbe4ab9e7
mlunzena <gitlab+elanev@luniki.de>
2020-04-07 07:21:48 +0200

    add link to video editor, refs #143

--- views/course/_episode.php
+++ views/course/_episode.php
@@ -148,6 +148,7 @@ $visibility_text = [
                                             'data-dialog'     => 'size=auto',
                                             'data-visibility' => $item['visibility'] ?: 'invisible'
                                         ]); ?>
+                                        <?= Studip\LinkButton::create($_('Schnitteditor öffnen'), $config['service_url'].'/admin-ng/index.html#!/events/events/'.$item['id'].'/tools/editor', ['target' => '_blank']); ?>
                                 <? endif; ?>
                             </div>
                         </div>
