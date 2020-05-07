bc63e02207678889dd507419cef622eaedec9f1d
Till Glöggler <till@gundk.it>
2020-05-07 12:36:51 +0200

    add migration to remove obsolete cronjob

---
+++ migrations/031_remove_chunked_upload.php
@@ -0,0 +1,24 @@
+<?php
+
+class RemoveChunkedUpload extends Migration
+{
+    const FILENAMES = [
+        'public/plugins_packages/elan-ev/OpenCast/cronjobs/try_reupload_failed_jobs.php'
+    ];
+
+    public function description()
+    {
+        return 'remove cronjob for reuploading filed media uploads';
+    }
+
+    public function up()
+    {
+        foreach (self::FILENAMES as $filename) {
+            if ($task_id = CronjobTask::findByFilename($filename)[0]->task_id) {
+                $task_id = CronjobScheduler::getInstance()->unregisterTask($task_id);
+            }
+        }
+    }
+
+    function down() {}
+}
