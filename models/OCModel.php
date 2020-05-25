0d58f7d5493ea36e64ca36df4400c76da000ca0d
Till Glöggler <till@gundk.it>
2020-05-25 12:20:20 +0200

    fix number of bound variables

--- models/OCModel.php
+++ models/OCModel.php
@@ -683,7 +683,7 @@ class OCModel
     static function getCoursesForEpisode($episode_id){
         $stmt = DBManager::get()->prepare("SELECT seminar_id
             FROM oc_seminar_episodes
-            WHERE series_id = ? AND episode_id = ?");
+            WHERE episode_id = ?");
         $stmt->execute([$episode_id]);
 
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
