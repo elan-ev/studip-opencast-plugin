a919215572d532cb3b9f9728254c2bc54d2dce1a
ssrahn <srahn@uni-osnabrueck.de>
2021-01-11 22:05:00 +0100

    Endpoint fehlt noch (?)

--- classes/OCRestClient/AdminNgClient.php
+++ classes/OCRestClient/AdminNgClient.php
@@ -17,5 +17,21 @@ class AdminNgClient extends OCRestClient
         }
     }
     
-    # in SchedulerClient -> deleteEventForSeminar
+    /**
+     * delelteEpisode -  retracts and deletes an episode
+     *
+     * @param string $episode_id - episode identifier
+     *
+     * @return bool success or not
+     */
+    public function deleteEpisode($episode_id)
+    {
+        $result = $this->deleteJSON('/event/' . $episode_id, true);
+        trigger_error($result[1], E_USER_ERROR);
+        
+        if (in_array($result[1], [200, 202])) {
+            return true;
+        }
+        return false;
+    }
 }
