2a9f78eb522aca36070ea8414e53769b93b1503e
Rainer Rillke <rillke@wikipedia.de>
2021-03-29 14:22:19 +0200

    s/Datenschutz*/Nutzungsvereinbarung/
    Resolves #453

--- views/course/access_denied.php
+++ views/course/access_denied.php
@@ -1,1 +1,1 @@
-<?= MessageBox::info($_('Der Zugriff auf die Videos dieser Veranstaltung wurde noch nicht freigegeben.')) ?>
+<?= MessageBox::info($_('Der Zugriff auf die Videos dieser Veranstaltung wurde noch nicht durch Ihre Lehrenden freigegeben. Lehrende müssen die Nutzungsvereinbarung für Opencast akzeptieren.')) ?>
