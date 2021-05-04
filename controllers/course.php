7366712ad91938f9dec88f4421c7d218b9b3189a
ssrahn <41595254+ssrahn@users.noreply.github.com>
2021-05-04 10:04:54 +0200

    Log Eintrag für TOS (#474)
    * log, wenn TOS akzeptiert
    
    * log for reject. cleaner output

--- controllers/course.php
+++ controllers/course.php
@@ -240,6 +240,7 @@ class CourseController extends OpencastController
 
     public function accept_tos_action()
     {
+        StudipLog::log('OC_TOS', $this->course_id, null, "akzeptiert");
         if (!Config::get()->OPENCAST_SHOW_TOS || !OCPerm::editAllowed($this->course_id)) {
             return $this->redirect('course/index');
         }
@@ -257,6 +258,7 @@ class CourseController extends OpencastController
 
     public function withdraw_tos_action()
     {
+        StudipLog::log('OC_TOS', $this->course_id, null, "abgelehnt");
         if (!Config::get()->OPENCAST_SHOW_TOS || !OCPerm::editAllowed($this->course_id)) {
             return $this->redirect('course/index');
         }
---
+++ migrations/038_add_log_tos.php
@@ -0,0 +1,23 @@
+<?php
+
+class AddLogTos extends Migration {
+
+    function up()
+    {
+        $db = DBManager::get();
+        $query = $db->prepare("INSERT INTO log_actions (action_id, name, description, info_template, active) VALUES (?, ?, ?, ?, ?)");
+
+        $query->execute(array(md5('OC_TOS'), 'OC_TOS', 'Opencast: TOS akzeptiert/abgelehnt', '%user hat TOS %info in %sem(%affected)', 1));
+    }
+
+    function down()
+    {
+        $db = DBManager::get();
+        $query = $db->prepare("DELETE FROM log_actions WHERE action_id = ?");
+        $query2 = $db->prepare("DELETE FROM log_events WHERE action_id = ?");
+        $query->execute(array(md5('OC_TOS')));
+        $query2->execute(array(md5('OC_TOS')));
+    }
+}
+
+?>
