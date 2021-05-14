a0c1cf194cd2841efd2a739969575220d4d4a743
ssrahn <srahn@uni-osnabrueck.de>
2021-05-14 19:07:15 +0200

    remove db table

---
+++ migrations/041_remove_oc_seminar_workflows.php
@@ -0,0 +1,20 @@
+<?php
+class RemoveOcSeminarWorkflows extends Migration
+{
+
+    function up()
+    {
+        DBManager::get()->query("DROP TABLE IF EXISTS `oc_seminar_workflows`;");
+    }
+
+    function down() {
+        DBManager::get()->query("CREATE TABLE IF NOT EXISTS `oc_seminar_workflows` (
+            `config_id` INT NOT NULL DEFAULT 1,
+            `workflow_id` varchar(255) NOT NULL,
+            `seminar_id` varchar(32) NOT NULL,
+            `user_id` varchar(32) NOT NULL,
+            `mkdate` INT DEFAULT 0,
+            PRIMARY KEY (`workflow_id`)
+            ) ROW_FORMAT=DYNAMIC;");
+    }
+}
