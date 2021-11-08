20a1a5e4df5ca7fe62c6519a52245d7223d7afcd
FarbodZamani <53179227+ferishili@users.noreply.github.com>
2021-11-08 15:12:33 +0100

    Courseware block lti (#537)
    * fixes the lti module in courseware block
    
    * cleaning up

--- controllers/ajax.php
+++ controllers/ajax.php
@@ -3,6 +3,9 @@
 use Opencast\Models\OCConfig;
 use Opencast\Models\OCSeminarEpisodes;
 
+use Opencast\LTI\OpencastLTI;
+use Opencast\LTI\LtiLink;
+
 class AjaxController extends OpencastController
 {
     public function index_action()
@@ -92,5 +95,45 @@ class AjaxController extends OpencastController
         }
 
         $this->render_json(array_values($result));
+    }
+
+    public function getltidata_action($course_id, $series_id)
+    {
+        $config     = OCConfig::getConfigForCourse($course_id);
+
+        $current_user_id = $GLOBALS['auth']->auth['uid'];
+        $lti_link        = new LtiLink(
+            OpencastLTI::getSearchUrl($course_id),
+            $config['lti_consumerkey'],
+            $config['lti_consumersecret']
+        );
+
+        if (OCPerm::editAllowed($course_id, $current_user_id)) {
+            $role = 'Instructor';
+        } else if ($GLOBALS['perm']->have_studip_perm('autor', $course_id, $current_user_id)) {
+            $role = 'Learner';
+        }
+
+        $lti_link->setUser($current_user_id, $role, True);
+        $lti_link->setCourse($course_id);
+        $lti_link->setResource(
+            $series_id,
+            'series',
+            'view complete series for course'
+        );
+
+        $launch_data = $lti_link->getBasicLaunchData();
+        $signature   = $lti_link->getLaunchSignature($launch_data);
+
+        $launch_data['oauth_signature'] = $signature;
+
+        $lti_data = json_encode($launch_data);
+        $lti_url  = $lti_link->getLaunchURL();
+
+        $result = [
+            'lti_url' => $lti_url,
+            'lti_data' => $lti_data
+        ];
+        $this->render_json($result);
     }
 }
