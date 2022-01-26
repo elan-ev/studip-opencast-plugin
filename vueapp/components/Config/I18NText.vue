3448f633cf3cc0bde867c6483a8a1a67aad8ff86
tgloeggl <tgloeggl@uos.de>
2022-01-26 16:05:57 +0100

    correctly switch between languages in i18n wysiwyg

--- vueapp/components/Config/I18NText.vue
+++ vueapp/components/Config/I18NText.vue
@@ -1,32 +1,43 @@
 <template>
     <div>
-        <div>
-            <span v-translate>
-                Zu bearbeitende Sprache auswählen
-            </span>
-        </div>
+        <label>
+            <div class="i18n_group">
+                <div v-for="lang in languages"
+                    class="i18n"
+                    :style="{
+                        display: lang.name != selectedLang.name ? 'none' : 'block'
+                    }"
+                    :data-lang="lang.name"
+                    :data-icon="`url(` + getLangImage(lang) + `)`">
+                    <textarea
+                        v-model="text[lang.name]"
+                        :id="`studip_wysiwyg_` + lang.name"
+                        :ref="`studip_wysiwyg_` + lang.name"
+                        class="studip-wysiwyg"
+                        @input="updateValue($event.target.value)"
+                    >
+                    </textarea>
+                </div>
 
-        <section class="hgroup size-s">
-            <label v-for="lang in languages">
-                <input type="radio"
-                    :name="content.name + '_lang'" :value="lang"
+                <select tabindex="-1" class="i18n"
+                    :style="`background-image: url(` + getLangImage(selectedLang) + `)`"
                     v-model="selectedLang"
+                    @change="initCKE()"
                 >
-                <img
-                    class="oc--lang"
-                    :src="getLangImage(lang)"
-                    width="24px"
-                >
-            </label>
-        </section>
-
-        <!--<studip-wysiwyg v-model="currentText"></studip-wysiwyg>-->
+                    <option
+                        v-for="lang in languages"
+                        :value="lang"
+                        :style="`background-image: url(` + getLangImage(lang) + `)`"
+                    >
+                        {{ lang.name}}
+                    </option>
+                </select>
+            </div>
+        </label>
     </div>
 </template>
 
 <script>
-
-
 export default {
     name: 'I18NText',
 
@@ -34,15 +45,72 @@ export default {
 
     data() {
         return {
-            languages: ['de', 'en'],
-            selectedLang: 'de',
-            currentText: 'test'
+            languages: [{
+                'name': 'Deutsch',
+                'picture': 'lang_de.gif'
+            },{
+                'name': 'English',
+                'picture': 'lang_en.gif'
+            }],
+
+            selectedLang: {
+                'name': 'Deutsch',
+                'picture': 'lang_de.gif'
+            },
+            text: {
+                'Deutsch': 'deutscher Text',
+                'English': 'english text'
+            },
+            fallbackActive: false,
+            wysiwyg_editor: null
         }
     },
 
+    mounted() {
+        let ckeInit = this.initCKE();
+        if (!ckeInit) {
+            this.fallbackActive = true;
+        }
+    },
+
+
     methods: {
         getLangImage(lang) {
-            return ASSETS_URL + 'images/languages/lang_' + lang + '.gif';
+            return ASSETS_URL + 'images/languages/' + lang.picture;
+        },
+
+        async initCKE() {
+            if (!STUDIP.wysiwyg_enabled) {
+                return false;
+            }
+
+            let view = this;
+
+            if (view.wysiwyg_editor !== null) {
+                await view.wysiwyg_editor.destroy();
+            }
+
+            let textarea = view.$refs['studip_wysiwyg_' + view.selectedLang.name][0];
+
+
+            await STUDIP.wysiwyg.replace(textarea);
+            view.wysiwyg_editor = CKEDITOR.instances[textarea.id];
+
+            view.wysiwyg_editor.on('blur', function() {
+                //console.log('cke blur');
+            });
+
+            view.wysiwyg_editor.on('change', function() {
+                view.text[view.selectedLang.name] = view.wysiwyg_editor.getData();
+            });
+
+            return true;
+        },
+
+        updateValue(value) {
+            if (this.fallbackActive) {
+                this.$emit('input', value);
+            }
         }
     }
 }
