a222f87fefac7a4af5f426790cfad707d935f5e4
Sebastian Biller <s.biller@tu-braunschweig.de>
2022-04-09 10:13:44 +0200

    undo

--- views/course/_episode.php
+++ views/course/_episode.php
@@ -168,7 +168,7 @@ $sort_orders = Pager::getSortOptions();
                                         $_('Technisches Feedback'),
                                         'mailto:' . Config::get()->OPENCAST_SUPPORT_EMAIL
                                             . '?subject=[Opencast] Feedback&body=%0D%0A%0D%0A%0D%0ALinks zum betroffenen Video:%0D%0A'
-                                            . PluginEngine::getLink('opencast/course/index/' . $item['id'], ['cid' => $course_id]) ."%0D%0A"
+                                            . $controller->link_for('course/index/' . $item['id']) ."%0D%0A"
                                             . $video_url . $item['id'],
                                         [
                                             'target' => '_blank',
