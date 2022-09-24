e770e3c44e941f89121c70d7eefa821eb46fe9f2
Till Glöggler <till@gundk.it>
2022-09-24 16:44:21 +0200

    extract role check to separate function

--- lib/Providers/Perm.php
+++ lib/Providers/Perm.php
@@ -14,32 +14,18 @@ class Perm
      */
     public static function editAllowed($context_id = null, $user_id = null)
     {
-        global $user;
-
         if (is_null($context_id)) {
             $context_id = \Context::getId();
-        }
-
-        if (is_null($user_id)) {
-            $user_id = $user->id;
         }
 
         //get permission level for editing episodes
         $requiredPerm = \Config::get()->OPENCAST_TUTOR_EPISODE_PERM ? 'tutor' : 'dozent';
 
         if (\Config::get()->OPENCAST_MEDIA_ROLES) {
-            // check, if current user has role "Medientutor"
-            $check_user = \User::find($user_id);
-
-            foreach ($check_user->getRoles() as $role) {
-                // media tutors are allowed to upload videos in any seminar where they are tutor
-                if ($role->rolename == 'Medientutor') {
-                    $requiredPerm = 'tutor';
-                }
+            if (self::hasRole('Medientutor', $user_id)) {
+                $requiredPerm = 'tutor';
             }
         }
-
-        // TODO: allow authors editing their own videos as well
 
         return $GLOBALS['perm']->have_studip_perm($requiredPerm, $context_id, $user_id);
     }
@@ -60,5 +46,26 @@ class Perm
         if (!self::editAllowed($context_id, $user_id)) {
             throw new AccessDeniedException('Sie haben keine Berechtigung zum Zugriff auf diese Funktion.');
         }
+    }
+
+    public static function hasRole($check_role, $user_id = null)
+    {
+        global $user;
+
+        if (is_null($user_id)) {
+            $user_id = $user->id;
+        }
+
+        // check, if current user has role "Medientutor"
+        $check_user = \User::find($user_id);
+
+        foreach ($check_user->getRoles() as $role) {
+            // media tutors are allowed to upload videos in any seminar where they are tutor
+            if ($role->rolename == $check_role) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
