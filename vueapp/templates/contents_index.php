d90b0954f249f4c174ac8a04ee25a9ecb30fafe3
tgloeggl <tgloeggl@uos.de>
2022-10-13 12:51:47 +0200

    cid does not break content page of videos anymore

--- vueapp/templates/contents_index.php
+++ vueapp/templates/contents_index.php
@@ -4,7 +4,7 @@
 
 <script type="text/javascript">
     window.OpencastPlugin = {
-        API_URL    : '<?= PluginEngine::getURL('opencast', [], 'api') ?>',
+        API_URL    : '<?= PluginEngine::getURL('opencast', [], 'api', true) ?>',
         IMAGE      : '<?= Assets::url('images/icons/') ?>',
         ICON_URL   : '<?= Assets::url('images/icons/') ?>',
         ASSETS_URL : '<?= Assets::url('') ?>',
