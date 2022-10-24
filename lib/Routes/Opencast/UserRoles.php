f53f3b11026d270b536b0957537919008780a5ee
tgloeggl <tgloeggl@uos.de>
2022-10-24 17:49:38 +0200

    fix role listing for users

--- lib/Routes/Opencast/UserRoles.php
+++ lib/Routes/Opencast/UserRoles.php
@@ -41,19 +41,55 @@ class UserRoles extends OpencastController
             } else {
                 // get all videos the user has permissions on
                 foreach(VideosUserPerms::findByUser_id($user_id) as $vperm) {
+                    if (!$vperm->video->episode) continue;
+
                     if ($vperm->perm == 'owner' || $vperm->perm == 'write') {
-                        $roles[] = 'STUDIP_' . $vperm->video->episode . '_write';
+                        $roles[$vperm->video->episode . '_write'] = 'STUDIP_' . $vperm->video->episode . '_write';
                     } else {
-                        $roles[] = 'STUDIP_' . $vperm->video->episode . '_read';
+                        $roles[$vperm->video->episode . '_read'] = 'STUDIP_' . $vperm->video->episode . '_read';
                     }
                 }
 
-                foreach (Videos::findByFilter(new Filter([
+                // find videos with direct access
+                foreach (Videos::getUserVideos(new Filter([
                     'limit'  => -1
                 ])) as $video) {
-                    $roles[] = 'STUDIP_' . $vperm->video->episode . '_read';
+                    if (!$vperm->video->episode) continue;
+
+                    $roles[$vperm->video->episode . '_read'] = 'STUDIP_' . $vperm->video->episode . '_read';
                 }
 
+
+                // first, get all courses user has access to
+                $stmt = \DBManager::get()->prepare("SELECT seminar_id FROM seminar_user
+                    WHERE user_id = ?");
+
+                $stmt->execute([$user_id]);
+                $courses = $stmt->fetchAll(\PDO::FETCH_COLUMN);
+
+                // find videos in seminars
+                $stmt = \DBManager::get()->prepare("SELECT episode FROM oc_video_seminar
+                    INNER JOIN oc_video ON (oc_video.id = video_id)
+                    WHERE seminar_id IN (:courses)");
+                $stmt->bindValue(':courses', $courses, \StudipPDO::PARAM_ARRAY);
+                $stmt->execute();
+
+                foreach ($stmt->fetchAll(\PDO::FETCH_COLUMN) as $episode) {
+                    $roles[$episode . '_read'] = 'STUDIP_' . $episode . '_read';
+                }
+
+
+                // find videos in accessible playlists
+                $stmt = \DBManager::get()->prepare("SELECT episode FROM oc_playlist_seminar
+                    INNER JOIN oc_playlist_video USING (playlist_id)
+                    INNER JOIN oc_video ON (oc_video.id = oc_playlist_video.video_id)
+                    WHERE seminar_id IN (:courses)");
+                $stmt->bindValue(':courses', $courses, \StudipPDO::PARAM_ARRAY);
+                $stmt->execute();
+
+                foreach ($stmt->fetchAll(\PDO::FETCH_COLUMN) as $episode) {
+                    $roles[$episode . '_read'] = 'STUDIP_' . $episode . '_read';
+                }
             }
         }
 
