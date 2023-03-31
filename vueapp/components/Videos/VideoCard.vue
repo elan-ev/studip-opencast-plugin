7a973cdd6cbb5caba5e85bb6522099a0dec56d52
srahn <srahn@uni-osnabrueck.de>
2023-03-31 14:13:32 +0200

    implement episode availability

--- vueapp/components/Videos/VideoCard.vue
+++ vueapp/components/Videos/VideoCard.vue
@@ -7,7 +7,7 @@
 
             <div class="oc--flexitem oc--flexplaycontainer">
                 <div class="oc--playercontainer">
-                    <a v-if="event.publication && event.preview" @click="redirectAction(`/video/` + event.token)" target="_blank">
+                    <a v-if="event.publication && event.preview && event.available" @click="redirectAction(`/video/` + event.token)" target="_blank">
                         <span class="oc--previewimage">
                             <img class="oc--previewimage"
                                 :src="event.preview.player ? event.preview.player : event.preview.search"
@@ -25,6 +25,9 @@
                             </span>
                         </span>
                     </a>
+                    <span v-else-if="!event.available" class="oc--unavailable">
+                        {{ $gettext("Video nicht verfügbar") }}
+                    </span>
                     <span v-else class="oc--previewimage">
                         <img class="oc--previewimage" :src="preview" height="200"/>
                         <!-- <p>No video uploaded</p> -->
