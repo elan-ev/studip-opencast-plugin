12911f08fa6c954c7f41c21003bdbd443870d0cb
Till Glöggler <till@gundk.it>
2023-08-08 10:52:06 +0200

    update package.json and remove wrong version of courseware plugin

--- vueapp/courseware/register.js
+++
@@ -1,7 +0,0 @@
-import CoursewareOpencastBlock from "./courseware-plugin-opencast-video.vue";
-
-window.STUDIP.eventBus.on("courseware:init-plugin-manager", (pluginManager) => {
-    pluginManager.addBlock("courseware-plugin-opencast-video-block", CoursewareOpencastBlock);
-});
-
-export default CoursewareOpencastBlock;
