84ac67b88b45367fd582654fac5186233f941411
Dennis Benz <44410838+dennis531@users.noreply.github.com>
2024-01-25 12:39:04 +0100

    Show course names in playlist select dropdown, ref #794 (#889)
    Co-authored-by: Dennis Benz <dennis.benz@uos.de>

--- lib/Models/PlaylistSeminars.php
+++ lib/Models/PlaylistSeminars.php
@@ -217,10 +217,11 @@ class PlaylistSeminars extends \SimpleORMap
                 }
 
                 $courses[] = [
-                    'id'        => $course->id,
-                    'name'      => $course->getFullname('number-name'),
-                    'semester'  => $course->getFullname('sem-duration-name'),
-                    'lecturers' => $lecturers,
+                    'id'                    => $course->id,
+                    'name'                  => $course->getFullname('number-name'),
+                    'semester'              => $course->getFullname('sem-duration-name'),
+                    'end_semester_begin'    => $course->end_semester->beginn ?: 0,
+                    'lecturers'             => $lecturers,
                 ];
             }
         }
