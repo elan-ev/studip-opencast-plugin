5560911018f3ee86b180144081db8b17967154fd
Till Glöggler <till@gundk.it>
2024-05-13 14:26:14 +0200

    make routemap readable again

--- lib/RouteMap.php
+++ lib/RouteMap.php
@@ -54,177 +54,72 @@ class RouteMap
         $group->get("/videos", Routes\Video\VideoList::class);
 
         $group->put("/videos/{token}", Routes\Video\VideoUpdate::class);
-        $group->put(
-            "/videos/{token}/restore",
-            Routes\Video\VideoRestore::class
-        );
+        $group->put("/videos/{token}/restore", Routes\Video\VideoRestore::class);
         $group->delete("/videos/{token}", Routes\Video\VideoDelete::class);
         $group->post("/videos/{episode_id}", Routes\Video\VideoAdd::class);
 
         $group->post("/videos/{token}/report", Routes\Video\VideoReport::class);
 
-        $group->get(
-            "/videos/{token}/captions",
-            Routes\Video\VideoCaptions::class
-        );
-        $group->get(
-            "/videos/{token}/shares",
-            Routes\Video\VideoSharesList::class
-        );
-        $group->put(
-            "/videos/{token}/shares",
-            Routes\Video\VideoSharesUpdate::class
-        );
-        $group->post(
-            "/videos/{course_id}/copy",
-            Routes\Video\VideoCopyToCourse::class
-        );
+        $group->get("/videos/{token}/captions", Routes\Video\VideoCaptions::class);
+        $group->get("/videos/{token}/shares", Routes\Video\VideoSharesList::class);
+        $group->put("/videos/{token}/shares", Routes\Video\VideoSharesUpdate::class);
+        $group->post("/videos/{course_id}/copy", Routes\Video\VideoCopyToCourse::class);
 
         // Playlist routes
         $group->get("/playlists", Routes\Playlist\PlaylistList::class);
         $group->post("/playlists", Routes\Playlist\PlaylistAdd::class);
         $group->get("/playlists/{token}", Routes\Playlist\PlaylistShow::class);
-        $group->put(
-            "/playlists/{token}",
-            Routes\Playlist\PlaylistUpdate::class
-        );
-        $group->delete(
-            "/playlists/{token}",
-            Routes\Playlist\PlaylistDelete::class
-        );
-        $group->post(
-            "/playlists/{token}/copy",
-            Routes\Playlist\PlaylistCopy::class
-        );
+        $group->put("/playlists/{token}", Routes\Playlist\PlaylistUpdate::class);
+        $group->delete("/playlists/{token}", Routes\Playlist\PlaylistDelete::class);
+        $group->post("/playlists/{token}/copy", Routes\Playlist\PlaylistCopy::class);
 
-        $group->get(
-            "/playlists/{token}/videos",
-            Routes\Playlist\PlaylistVideoList::class
-        );
+        $group->get("/playlists/{token}/videos", Routes\Playlist\PlaylistVideoList::class);
 
-        $group->put(
-            "/playlists/{token}/video/{vid_token}",
-            Routes\Playlist\PlaylistAddVideo::class
-        );
-        $group->delete(
-            "/playlists/{token}/video/{vid_token}",
-            Routes\Playlist\PlaylistRemoveVideo::class
-        );
+        $group->put("/playlists/{token}/video/{vid_token}", Routes\Playlist\PlaylistAddVideo::class);
+        $group->delete("/playlists/{token}/video/{vid_token}", Routes\Playlist\PlaylistRemoveVideo::class);
 
-        $group->put(
-            "/playlists/{token}/user",
-            Routes\Playlist\PlaylistAddUser::class
-        );
-        $group->delete(
-            "/playlists/{token}/user/{username}",
-            Routes\Playlist\PlaylistRemoveUser::class
-        );
+        $group->put("/playlists/{token}/user", Routes\Playlist\PlaylistAddUser::class);
+        $group->delete("/playlists/{token}/user/{username}", Routes\Playlist\PlaylistRemoveUser::class);
 
-        $group->put(
-            "/playlists/{token}/positions",
-            Routes\Playlist\PlaylistUpdatePositions::class
-        );
+        $group->put("/playlists/{token}/positions", Routes\Playlist\PlaylistUpdatePositions::class);
 
-        $group->get(
-            "/playlists/{token}/courses",
-            Routes\Playlist\PlaylistCourses::class
-        );
-        $group->put(
-            "/playlists/{token}/courses",
-            Routes\Playlist\PlaylistAddToCourse::class
-        );
+        $group->get("/playlists/{token}/courses", Routes\Playlist\PlaylistCourses::class);
+        $group->put("/playlists/{token}/courses", Routes\Playlist\PlaylistAddToCourse::class);
 
         // Schedule and Playlists
-        $group->post(
-            "/playlists/{token}/schedule/{course_id}/{type}",
-            Routes\Playlist\PlaylistScheduleUpdate::class
-        );
+        $group->post("/playlists/{token}/schedule/{course_id}/{type}", Routes\Playlist\PlaylistScheduleUpdate::class);
 
         // Course routes
         $group->get("/courses", Routes\Course\MyCourseList::class);
 
-        $group->get(
-            "/courses/{course_id}/videos",
-            Routes\Course\CourseVideoList::class
-        );
+        $group->get("/courses/{course_id}/videos", Routes\Course\CourseVideoList::class);
 
-        $group->get(
-            "/courses/{course_id}/config",
-            Routes\Course\CourseConfig::class
-        );
-        $group->get(
-            "/courses/{course_id}/playlists",
-            Routes\Course\CourseListPlaylist::class
-        );
+        $group->get("/courses/{course_id}/config", Routes\Course\CourseConfig::class);
+        $group->get("/courses/{course_id}/playlists", Routes\Course\CourseListPlaylist::class);
 
-        $group->get(
-            "/courses/{course_id}/{semester_filter}/schedule",
-            Routes\Course\CourseListSchedule::class
-        );
+        $group->get("/courses/{course_id}/{semester_filter}/schedule", Routes\Course\CourseListSchedule::class);
 
-        $group->post(
-            "/courses/{course_id}/playlist/{token}",
-            Routes\Course\CourseAddPlaylist::class
-        );
-        $group->put(
-            "/courses/{course_id}/playlist/{token}",
-            Routes\Course\CourseUpdatePlaylist::class
-        );
-        $group->delete(
-            "/courses/{course_id}/playlist/{token}",
-            Routes\Course\CourseRemovePlaylist::class
-        );
+        $group->post("/courses/{course_id}/playlist/{token}", Routes\Course\CourseAddPlaylist::class);
+        $group->put("/courses/{course_id}/playlist/{token}", Routes\Course\CourseUpdatePlaylist::class);
+        $group->delete("/courses/{course_id}/playlist/{token}", Routes\Course\CourseRemovePlaylist::class);
 
-        $group->put(
-            "/courses/{course_id}/visibility/{visibility}",
-            Routes\Course\CourseSetVisibility::class
-        );
-        $group->put(
-            "/courses/{course_id}/upload/{upload}",
-            Routes\Course\CourseSetUpload::class
-        ); // TODO: document in api docs
+        $group->put("/courses/{course_id}/visibility/{visibility}", Routes\Course\CourseSetVisibility::class);
+        $group->put("/courses/{course_id}/upload/{upload}", Routes\Course\CourseSetUpload::class); // TODO: document in api docs
 
-        $group->get(
-            "/courses/videos",
-            Routes\Course\CourseListForUserVideos::class
-        );
-        $group->get(
-            "/courses/videos/playlist/{token}",
-            Routes\Course\CourseListForPlaylistVideos::class
-        );
+        $group->get("/courses/videos", Routes\Course\CourseListForUserVideos::class);
+        $group->get("/courses/videos/playlist/{token}", Routes\Course\CourseListForPlaylistVideos::class);
 
         // Schedule
-        $group->get(
-            "/schedule/{course_id}/{termin_id}",
-            Routes\Schedule\ScheduleShow::class
-        );
-        $group->post(
-            "/schedule/{course_id}/{termin_id}",
-            Routes\Schedule\ScheduleAdd::class
-        );
-        $group->post(
-            "/schedulebulk/{course_id}",
-            Routes\Schedule\ScheduleBulk::class
-        );
-        $group->put(
-            "/schedule/{course_id}/{termin_id}",
-            Routes\Schedule\ScheduleUpdate::class
-        );
-        $group->delete(
-            "/schedule/{course_id}/{termin_id}",
-            Routes\Schedule\ScheduleDelete::class
-        );
+        $group->get("/schedule/{course_id}/{termin_id}", Routes\Schedule\ScheduleShow::class);
+        $group->post("/schedule/{course_id}/{termin_id}", Routes\Schedule\ScheduleAdd::class);
+        $group->post("/schedulebulk/{course_id}", Routes\Schedule\ScheduleBulk::class);
+        $group->put("/schedule/{course_id}/{termin_id}", Routes\Schedule\ScheduleUpdate::class);
+        $group->delete("/schedule/{course_id}/{termin_id}", Routes\Schedule\ScheduleDelete::class);
 
         $group->get("/tags", Routes\Tags\TagListForUser::class);
         $group->get("/tags/videos", Routes\Tags\TagListForUserVideos::class);
-        $group->get(
-            "/tags/videos/playlist/{token}",
-            Routes\Tags\TagListForPlaylistVideos::class
-        );
-        $group->get(
-            "/tags/videos/course/{course_id}",
-            Routes\Tags\TagListForCourseVideos::class
-        );
+        $group->get("/tags/videos/playlist/{token}", Routes\Tags\TagListForPlaylistVideos::class);
+        $group->get("/tags/videos/course/{course_id}", Routes\Tags\TagListForCourseVideos::class);
 
         $group->get("/config/simple", Routes\Config\SimpleConfigList::class);
         $group->post("/log", Routes\Log\LogEntryCreate::class);
