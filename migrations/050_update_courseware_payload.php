13c1dc0fd66fc46592aaffca1b114cc46dc7722e
Till Glöggler <till@gundk.it>
2024-06-27 16:00:45 +0200

    remove regexp_replace from migration, since it does not work with mysql prior to 8

--- migrations/050_update_courseware_payload.php
+++ migrations/050_update_courseware_payload.php
@@ -9,12 +9,37 @@ class UpdateCoursewarePayload extends Migration
             PluginEngine::getLink('opencast', [], 'redirect/perform/video/')
         );
 
-        $stmt = $db->exec($query = "UPDATE cw_blocks
-            SET payload = REGEXP_REPLACE(payload, '\"url\":\".*\\\\/(.*?)\",\"', '\"url\":\"" .
-                $link
-            . "\\\\1\",\"')
+        $stmt = $db->prepare('UPDATE cw_blocks
+            SET payload = :payload
+            WHERE id = :id');
+
+        $result = $db->query("SELECT id, payload FROM cw_blocks
             WHERE block_type = 'plugin-opencast-video'
-        ");
+                AND payload NOT LIKE '%perform%'");
+
+        echo '<pre>';
+
+        while ($data = $result->fetch(PDO::FETCH_ASSOC)) {
+            $payload = json_decode($data['payload'], true);
+
+            $url = $payload['url'];
+            preg_match('/id=(.*)/', $url, $matches);
+
+            $video_id = null;
+
+            if ($matches[1]) {
+                $video_id = $matches[1];
+            }
+
+            if ($video_id) {
+                $payload['url'] = $link . $video_id;
+
+                $stmt->execute([
+                    ':payload' => json_encode($payload),
+                    ':id'      => $data['id']
+                ]);
+            }
+        }
     }
 
     public function down()
