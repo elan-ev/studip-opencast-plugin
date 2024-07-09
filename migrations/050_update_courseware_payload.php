9033ab98d269d8ab28c6cab26cdfdedd661fa89c
Till Glöggler <till@gundk.it>
2024-07-09 13:43:09 +0200

    remove url from opencast courseware block payload

--- migrations/050_update_courseware_payload.php
+++ migrations/050_update_courseware_payload.php
@@ -5,40 +5,22 @@ class UpdateCoursewarePayload extends Migration
     {
         $db = DBManager::get();
 
-        $link = str_replace('/', '\\/',
-            PluginEngine::getLink('opencast', [], 'redirect/perform/video/')
-        );
-
         $stmt = $db->prepare('UPDATE cw_blocks
             SET payload = :payload
             WHERE id = :id');
 
         $result = $db->query("SELECT id, payload FROM cw_blocks
-            WHERE block_type = 'plugin-opencast-video'
-                AND payload NOT LIKE '%perform%'");
-
-        echo '<pre>';
+            WHERE block_type = 'plugin-opencast-video'");
 
         while ($data = $result->fetch(PDO::FETCH_ASSOC)) {
             $payload = json_decode($data['payload'], true);
 
-            $url = $payload['url'];
-            preg_match('/id=(.*)/', $url, $matches);
+            unset($payload['url']);
 
-            $video_id = null;
-
-            if ($matches[1]) {
-                $video_id = $matches[1];
-            }
-
-            if ($video_id) {
-                $payload['url'] = $link . $video_id;
-
-                $stmt->execute([
-                    ':payload' => json_encode($payload),
-                    ':id'      => $data['id']
-                ]);
-            }
+            $stmt->execute([
+                ':payload' => json_encode($payload),
+                ':id'      => $data['id']
+            ]);
         }
     }
 
