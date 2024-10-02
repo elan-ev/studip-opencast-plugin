1ff395739e6d7b469c9a1cd6282bfaabdb898ff4
Till Glöggler <till@gundk.it>
2024-10-02 10:41:21 +0200

    fixes #1017

--- translate.sh
+++ translate.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Fix broken easygettext
+sed -i 's|cheerio/lib/utils|cheerio/utils|g' node_modules/easygettext/src/extract.js
+
+# First, run javascript extraction
+npx gettext-extract --attribute v-i18n --attribute v-translate --output locale/en/LC_MESSAGES/opencast_js.pot $(find vueapp -type f -name '*.vue')  $(find vueapp -type f -name '*.js')
+
+# Run standard gettext extraction
 PO=locale/en/LC_MESSAGES/opencast.po
 POTPHP=locale/en/LC_MESSAGES/opencast_php.pot
 POTJS=locale/en/LC_MESSAGES/opencast_js.pot
