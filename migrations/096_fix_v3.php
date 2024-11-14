d634dafd540b82c459efc29e6b1095fb5361af27
Till Glöggler <till@gundk.it>
2024-11-14 10:35:45 +0100

    fixes #1066

--- migrations/096_fix_v3.php
+++ migrations/096_fix_v3.php
@@ -29,13 +29,13 @@ class FixV3 extends Migration
         $new_plugin_id = $db->query("SELECT MAX(pluginid) FROM plugins")->fetchColumn() + 1;
 
         if (!empty($old_plugin_id) && !empty($new_plugin_id)) {
-            $stmt = $db->prepare("UPDATE tools_activated SET plugin_id = ':new' WHERE plugin_id = ':old'");
+            $stmt = $db->prepare("UPDATE tools_activated SET plugin_id = :new WHERE plugin_id = :old");
             $stmt->execute([
                 ':new' => $new_plugin_id,
                 ':old' => $old_plugin_id
             ]);
 
-            $stmt = $db->prepare("UPDATE plugins_activated SET pluginid = ':new' WHERE pluginid = ':old'");
+            $stmt = $db->prepare("UPDATE plugins_activated SET pluginid = :new WHERE pluginid = :old");
             $stmt->execute([
                 ':new' => $new_plugin_id,
                 ':old' => $old_plugin_id
