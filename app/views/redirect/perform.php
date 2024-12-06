0c703ce64d60500b3164dd534d45d2f9cb61c0ee
Dennis Benz <dennis.benz@uni-osnabrueck.de>
2024-12-06 09:28:37 +0100

    Add subtle loading spinner

--- app/views/redirect/perform.php
+++ app/views/redirect/perform.php
@@ -33,8 +33,44 @@
                 document.getElementById('ltiLaunchForm').submit();
             });
         </script>
+        <style>
+            html, body, form {
+                margin: 0;
+            }
+            .oc--loading-spinner {
+                height: 100vh;
+                margin: 0;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+            .oc--spinner {
+                transform: scale(0.5);
+            }
+            .oc--spinner:after {
+                content: " ";
+                display: block;
+                width: 64px;
+                height: 64px;
+                border-radius: 50%;
+                border: 6px solid;
+                border-color: #28497c transparent #28497c transparent;
+                animation: oc-spinner 1.2s linear infinite;
+            }
+            @keyframes oc-spinner {
+                0% {
+                    transform: rotate(0deg);
+                }
+                100% {
+                    transform: rotate(360deg);
+                }
+            }
+        </style>
     </head>
     <body>
+        <div class="oc--loading-spinner">
+            <div class="oc--spinner"></div>
+        </div>
         <form class="default" action="<?= $launch_url ?>" name="ltiLaunchForm" id="ltiLaunchForm" method="post" encType="application/x-www-form-urlencoded">
             <? foreach ($launch_data as $name => $value) : ?>
                 <input type="hidden" name="<?= htmlspecialchars($name) ?>" value="<?= htmlspecialchars($value) ?>">
