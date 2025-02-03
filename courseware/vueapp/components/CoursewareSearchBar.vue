354e6b67687c19f2372d5db7a164f65f1deec060
Dennis Benz <dennis.benz@uni-osnabrueck.de>
2025-02-03 12:37:27 +0100

    Show course videos in cw (#1132)
    * Show initially only course videos in courseware block
    
    * Fix video filter for admins
    
    Currently, admins could see all videos due to a missing join with the video table.
    
    * Show tutors, lecturers and admins their course videos and videos with perms in cw block and workplace
    
    * Add filter option for current course in cw block

--- courseware/vueapp/components/CoursewareSearchBar.vue
+++ courseware/vueapp/components/CoursewareSearchBar.vue
@@ -1,12 +1,24 @@
 <template>
     <div class="oc-cw-searchbar">
         <ul class="oc-cw-searchbar-container">
+            <li class="oc-cw-searchbar-token" v-show="showCurrentCourse">
+                <span>{{ $gettext('Diese Veranstaltung') }}</span>
+                <studip-icon
+                    shape="decline" role="clickable" class="oc-cw-remove-filter"
+                    @click="removeCourse"
+                    @blur="delayedHideCourseSelector"
+                />
+            </li>
             <li class="oc-cw-searchbar-input">
                 <input type="text"
                     ref="searchbar"
+                    v-on:keyup="hideCourseSelector"
                     v-on:keyup.enter="doSearch"
                     v-model="inputSearch"
                     :placeholder="$gettext('Suche...')"
+                    @focus="openCourseSelector"
+                    @click="openCourseSelector"
+                    @blur="delayedHideCourseSelector"
                     @input="doLiveSearch"
                 />
             </li>
@@ -19,6 +31,16 @@
                 />
             </li>
         </ul>
+
+        <div class="oc-cw-tokenselector" v-if="showCourseSelector"
+            :style="`left:` + selectorPos.left + `px; top:` + selectorPos.top + `px;`"
+        >
+            <ul>
+                <li @click="selectCourse">
+                    {{ $gettext('Diese Veranstaltung') }}
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -26,19 +48,64 @@
 export default {
     name: "CoursewareSearchBar",
 
+    props: {
+        currentCourseSelectable: {
+            type: Boolean,
+            default: true,
+        },
+        showCurrentCourse: {
+            type: Boolean,
+            default: true,
+        },
+    },
+
+    emits: ['doSearch'],
+
     data() {
         return {
             inputSearch: '',
+            showCourseSelector: false,
+            selectorPos: {
+                top: 0,
+                left: 0
+            },
             timer: null,
             delay: 800 //ms
         }
     },
 
     methods: {
+        openCourseSelector() {
+            // filter needs to be selectable and not active
+            if (this.currentCourseSelectable && !this.showCurrentCourse) {
+                this.showCourseSelector = true;
+
+                this.selectorPos.top = this.$refs.searchbar.offsetTop + 30;
+                this.selectorPos.left = this.$refs.searchbar.offsetLeft;
+            }
+        },
+
+        // this is done in order to avoid hiding (and therefore deactivating the token selector)
+        // before the click-events of the token selector had a chance to fire
+        delayedHideCourseSelector() {
+            let view = this;
+
+            window.setTimeout(() => {
+                view.hideCourseSelector();
+            }, 200);
+        },
+
+        hideCourseSelector() {
+            this.showCourseSelector = false;
+        },
+
         doSearch() {
             clearTimeout(this.timer);
 
-            this.$emit('doSearch', this.inputSearch);
+            this.$emit('doSearch', {
+                searchText: this.inputSearch,
+                showCurrentCourse: this.showCurrentCourse,
+            });
         },
 
         doLiveSearch() {
@@ -47,6 +114,23 @@ export default {
             this.timer = setTimeout(() => {
                 this.doSearch();
             }, this.delay);
+        },
+
+        selectCourse() {
+            this.showCurrentCourse = true;
+            this.hideCourseSelector();
+            this.doSearch();
+        },
+
+        removeCourse() {
+            this.showCurrentCourse = false;
+            this.doSearch();
+        }
+    },
+
+    updated() {
+        if (this.showCourseSelector) {
+            this.openCourseSelector();
         }
     },
 }
