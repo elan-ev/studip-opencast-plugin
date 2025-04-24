393db00d2effb0cc2d87110ecccf7fa440682238
FarbodZamani <53179227+ferishili@users.noreply.github.com>
2025-04-24 11:41:20 +0200

    Course Default Visibility feature, (#1324)
    This PR fixes #1316.
    It introduces a new course config via migration 109 called `OPENCAST_COURSE_DEFAULT_EPISODES_VISIBILITY`.
    Additionally, a new sidebar action is added which opens a dialog to change the default visibility of episodes in a course. It would apply to the episodes that have "default" as for their visibility!
    It works in a way, that it could be set to follow the default "global" config `OPENCAST_HIDE_EPISODES`, visible or hidden!

--- vueapp/components/Courses/CoursesSidebar.vue
+++ vueapp/components/Courses/CoursesSidebar.vue
@@ -218,6 +218,12 @@
                         </a>
                     </li>
                     <li v-if="canEdit">
+                        <a href="#" @click.prevent="$emit('changeDefaultVisibility')">
+                            <studip-icon style="margin-left: -20px;" :shape="changeDefaultVisibilityIcon" role="clickable"/>
+                            {{ $gettext('Standardsichtbarkeit Videos') }}
+                        </a>
+                    </li>
+                    <li v-if="canEdit">
                         <a href="#" @click.prevent="$emit('copyAll')">
                             <studip-icon style="margin-left: -20px;" shape="export" role="clickable"/>
                             {{ $gettext('Kursinhalte übertragen') }}
@@ -254,7 +260,8 @@ export default {
         'sortVideo',
         'saveSortVideo',
         'cancelSortVideo',
-        'changeDefaultPlaylist'
+        'changeDefaultPlaylist',
+        'changeDefaultVisibility'
     ],
 
     data() {
@@ -362,6 +369,16 @@ export default {
         hasDefaultPlaylist() {
             return this.course_config?.has_default_playlist;
         },
+
+        changeDefaultVisibilityIcon() {
+            let course_hide_episodes = false;
+            if (this.course_config.hasOwnProperty('course_hide_episodes')) {
+                course_hide_episodes = this.course_config.course_hide_episodes;
+            } else if (this.simple_config_list?.settings && this.simple_config_list.settings.hasOwnProperty('OPENCAST_HIDE_EPISODES')) {
+                course_hide_episodes = this.simple_config_list.settings.OPENCAST_HIDE_EPISODES;
+            }
+            return course_hide_episodes ? 'visibility-invisible' : 'visibility-visible';
+        }
     },
 
     methods: {
