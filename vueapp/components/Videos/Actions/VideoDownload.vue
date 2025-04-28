174087f588d860679265f432f72c99b319fe02cf
ferishili <zamanifarbod2@gmail.com>
2025-04-28 16:30:49 +0200

    Implement new video download functionality with progress indication and cancellation option,
    The download feature now informs users the status of the process as well as progress bar with cancel button.

--- vueapp/components/Videos/Actions/VideoDownload.vue
+++ vueapp/components/Videos/Actions/VideoDownload.vue
@@ -8,62 +8,41 @@
             @close="this.$emit('cancel')"
         >
             <template v-slot:dialogContent>
-                <div v-if="presenters.length">
-                    <h2>
-                        ReferentIn
-                    </h2>
-                    <a v-for="(media, index) in presenters" :key="index">
-                        <StudipButton @click.prevent="downloadFile(media, 'presenter', media.size)">
-                            {{ getMediaText(media) }}
-                        </StudipButton>
-
-                        <div class="oc--tooltip--copy">
-                            <div class="oc--tooltip--copy-success"
-                                :class="{
-                                    'oc--display--block': copied == media.url
-                                }"
-                            >
-                                {{ $gettext('Kopiert!') }}
-                            </div>
-
-                            <studip-icon
-                                v-if="event.visibility == 'public'"
-                                :title="$gettext('Link zur Mediendatei in die Zwischenablage kopieren')"
-                                @click="copyToClipboard(media.url)"
-                                :shape="copied == media.url ? 'accept' : 'copy'"
-                                :role="copied == media.url ? 'status-green' : 'clickable'"
+                <div class="oc--download-list-container">
+                    <div class="oc--download-list">
+                        <div v-if="presenters.length">
+                            <h2>
+                                ReferentIn
+                            </h2>
+                            <VideoDownloadItem
+                                v-for="(media, index) in tuned_presenters"
+                                :key="index"
+                                :media="media"
+                                :event="event"
+                                @performDownload="downloadFile(media, 'presenter', media.size)"
+                                @performAbortDownload="abortDownload(media)"
+                                @performCopyToClipboard="copyToClipboard(media.url)"
                             />
                         </div>
-                    </a>
-                </div>
-                <br>
-                <div v-if="presentations.length">
-                    <h2>
-                        Bildschirm
-                    </h2>
-                    <a v-for="(media, index) in presentations" :key="index">
-                        <StudipButton @click.prevent="downloadFile(media, 'presentation', media.size)">
-                            {{ getMediaText(media) }}
-                        </StudipButton>
-
-                        <div class="oc--tooltip--copy">
-                            <div class="oc--tooltip--copy-success"
-                                :class="{
-                                    'oc--display--block': copied == media.url
-                                }"
-                            >
-                                {{ $gettext('Kopiert!') }}
-                            </div>
-
-                            <studip-icon
-                                v-if="event.visibility == 'public'"
-                                :title="$gettext('Link zur Mediendatei in die Zwischenablage kopieren')"
-                                @click="copyToClipboard(media.url)"
-                                :shape="copied == media.url ? 'accept' : 'copy'"
-                                :role="copied == media.url ? 'status-green' : 'clickable'"
+                        <br>
+                        <div v-if="presentations.length">
+                            <h2>
+                                Bildschirm
+                            </h2>
+                            <VideoDownloadItem
+                                v-for="(media, index) in tuned_presentations"
+                                :key="index"
+                                :media="media"
+                                :event="event"
+                                @performDownload="downloadFile(media, 'presentation', media.size)"
+                                @performAbortDownload="abortDownload(media)"
+                                @performCopyToClipboard="copyToClipboard(media.url)"
                             />
                         </div>
-                    </a>
+                    </div>
+                    <div class="oc--download-messages">
+                        <MessageList :float="true" :dialog="true" />
+                    </div>
                 </div>
             </template>
         </StudipDialog>
@@ -74,6 +53,8 @@
 import StudipDialog from '@studip/StudipDialog'
 import StudipButton from '@studip/StudipButton'
 import StudipIcon from '@studip/StudipIcon';
+import VideoDownloadItem from '@/components/Videos/Actions/VideoDownloadItem.vue';
+import MessageList from "@/components/MessageList";
 
 import axios from "@/common/axios.service";
 
@@ -83,7 +64,9 @@ export default {
     components: {
         StudipDialog,
         StudipButton,
-        StudipIcon
+        StudipIcon,
+        VideoDownloadItem,
+        MessageList
     },
 
     props: ['event'],
@@ -96,20 +79,98 @@ export default {
         }
     },
 
+    computed: {
+        tuned_presentations() {
+            let tuned = this.presentations.map((media) => {
+                media.loading = false;
+                media.download_controller = new AbortController();
+                media.progress = 0;
+                return media;
+            });
+            return tuned
+        },
+
+        tuned_presenters() {
+            let tuned = this.presenters.map((media) => {
+                media.loading = false;
+                media.download_controller = new AbortController();
+                media.progress = 0;
+                return media;
+            });
+            return tuned
+        }
+    },
+
     methods: {
+        abortDownload(media) {
+            if (media?.loading == true) {
+                media.download_controller.abort();
+                media.loading = false;
+            }
+        },
         async downloadFile(media, type, index) {
+            if (media?.loading == true) {
+                return;
+            }
+            this.$store.dispatch('clearMessages', true);
+            let view = this;
+
             let url = window.OpencastPlugin.REDIRECT_URL + '/download/' + this.event.token + '/' + type + '/' + index;
+            media.loading = true;
+            media.progress = 0;
+
+            let dummy_download_link = null;
 
             axios.get(url, {
-                responseType: 'blob'
+                responseType: 'blob',
+                signal: media.download_controller.signal,
+                onDownloadProgress: (progressEvent) => {
+                    let percentage = Math.round(
+                        (progressEvent.loaded * 100) / index
+                    );
+                    if (percentage > 100) {
+                        percentage = 100;
+                    }
+                    media.progress = percentage;
+                }
             }).then(response => {
                 const blob = new Blob([response.data]);
-                const link = document.createElement('a');
-                link.href = URL.createObjectURL(blob);
-                link.download = this.getFileName(media);
-                link.click();
-                URL.revokeObjectURL(link.href);
-            }).catch(console.error);
+                dummy_download_link = document.createElement('a');
+                dummy_download_link.href = URL.createObjectURL(blob);
+                dummy_download_link.download = this.getFileName(media);
+                dummy_download_link.click();
+                view.$store.dispatch('addMessage', {
+                    'type': 'success',
+                    'text': this.$gettext('Herunterladen des Mediums abgeschlossen.'),
+                    'dialog': true
+                })
+
+            }).catch((err) => {
+                let message = {
+                    type: 'error',
+                    text: this.$gettext('Herunterladen des Mediums fehlgeschlagen!'),
+                    'dialog': true
+                }
+                if (axios.isCancel(err)) {
+                    message.type = 'warning';
+                    message.text = this.$gettext('Herunterladen des Mediums abgebrochen!');
+                }
+                view.$store.dispatch('addMessage', message)
+            })
+            .finally(() => {
+                this.resetMediaDownloadProps(media);
+                if (dummy_download_link) {
+                    URL.revokeObjectURL(dummy_download_link.href);
+                    dummy_download_link.remove();
+                    dummy_download_link = null;
+                }
+            });
+        },
+
+        resetMediaDownloadProps(media) {
+            media.loading = false;
+            media.download_controller = new AbortController();
+            media.progress = 0;
         },
 
         getFileName(media) {
@@ -117,26 +178,6 @@ export default {
             res = res.replace(' * ', ' x ').replace(/\s+/g, '');
             let ext = media.url.split(".").pop();
             return this.event.title + ' (' + res + ').' + ext;
-        },
-
-        getMediaText(media) {
-            var text = media?.info || '';
-            text = text.replace(' * ', ' x ');
-            var size = media?.size || 0;
-
-            if (size == 0) {
-                return text;
-            }
-
-            if (size > 1000) {
-                size = Math.round(size/1000 * 10) / 10
-                text = text + ' (' + size + ' MB)'
-            } else {
-                size = Math.round(size * 10) / 10
-                text = text + ' (' + size + ' KB)'
-            }
-
-            return text
         },
 
         extractDownloads() {
---
+++ vueapp/components/Videos/Actions/VideoDownloadItem.vue
@@ -0,0 +1,87 @@
+<template>
+    <a class="oc--download-item">
+        <div class="oc--download-btn-container">
+            <div v-if="media?.loading" class="oc--download-spinner-container">
+                <div class="oc--spinner"></div>
+            </div>
+            <StudipButton class="oc--download-btn" @click.prevent="$emit('performDownload')"
+                :disabled="media?.loading"
+            >
+                {{ getMediaText(media) }}
+            </StudipButton>
+            <studip-icon shape="decline"
+                :role="media?.loading ? 'clickable' : 'inactive'"
+                class="oc--download-cancel"
+                :class="media?.loading ? 'active' : 'inactive'"
+                :title="$gettext('Abbrechen')"
+                @click="$emit('performAbortDownload')"
+            />
+        </div>
+
+        <div class="oc--download-info-container">
+            <ProgressBar v-if="media?.loading" :progress="media.progress" :minimal="true" />
+        </div>
+
+        <div class="oc--tooltip--copy">
+            <div class="oc--tooltip--copy-success"
+                :class="{
+                    'oc--display--block': copied == media.url
+                }"
+            >
+                {{ $gettext('Kopiert!') }}
+            </div>
+
+            <studip-icon
+                v-if="event.visibility == 'public'"
+                :title="$gettext('Link zur Mediendatei in die Zwischenablage kopieren')"
+                @click="$emit('performCopyToClipboard')"
+                :shape="copied == media.url ? 'accept' : 'copy'"
+                :role="copied == media.url ? 'status-green' : 'clickable'"
+            />
+        </div>
+    </a>
+</template>
+
+<script>
+import StudipButton from '@studip/StudipButton'
+import StudipIcon from '@studip/StudipIcon';
+import ProgressBar from '@/components/ProgressBar'
+
+export default {
+    name: 'VideoDownloadItem',
+
+    components: {
+        StudipButton,
+        StudipIcon,
+        ProgressBar
+    },
+
+    props: ['media', 'event'],
+
+    emits: ['performDownload', 'performAbortDownload', 'performCopyToClipboard'],
+
+    methods: {
+        getMediaText(media) {
+            var text = media?.info || '';
+            text = text.replace(' * ', ' x ');
+            var size = parseInt(media?.size, 10) || 0;
+            if (size == 0) {
+                return text;
+            }
+
+            let kbtobytes = 1024;
+            let mbtobytes = 1000 * kbtobytes;
+
+            if (size >= mbtobytes) {
+                let megabytes = (size / mbtobytes).toFixed(1);
+                text = text + ' (' + megabytes + ' MB)'
+            } else {
+                let kilobytes = (size / kbtobytes).toFixed(1);
+                text = text + ' (' + kilobytes + ' KB)'
+            }
+
+            return text
+        },
+    },
+}
+</script>
