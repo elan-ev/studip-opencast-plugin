d8f88f259e879234960bc07be7f0cc77b014b8b2
Ron Lucke <lucke@elan-ev.de>
2025-06-23 11:42:59 +0200

    fix oc contentbar

--- vueapp/components/Layouts/ContentBar.vue
+++ vueapp/components/Layouts/ContentBar.vue
@@ -1,16 +1,9 @@
 <template>
     <div class="oc--content-bar">
-        <header>
-            <div class="oc--content-bar-header-buttons-left"><slot name="buttons-left"></slot></div>
-            <div class="oc--content-bar-header-title"><slot name="title"></slot></div>
-            <div class="oc--content-bar-header-buttons-right"><slot name="buttons-right"></slot></div>
-        </header>
-        <nav>
-            <div class="oc--content-bar-nav"><slot name="nav"></slot></div>
-            <div class="oc--content-bar-search"><slot name="search"></slot></div>
-        </nav>
+        <div class="oc--content-bar-header-title"><slot name="title"></slot></div>
+        <div class="oc--content-bar-nav"><slot name="nav"></slot></div>
+        <div class="oc--content-bar-search"><slot name="search"></slot></div>
     </div>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
