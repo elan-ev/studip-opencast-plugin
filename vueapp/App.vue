bae5fbd0f3f03562016d409427c81eab9d8f4f51
Ron Lucke <lucke@elan-ev.de>
2025-09-24 11:53:13 +0200

    clean up app

--- vueapp/App.vue
+++ vueapp/App.vue
@@ -2,19 +2,14 @@
     <div class="container" id="opencast">
         <div class="content">
             <router-view />
+            <VideoDrawer />
         </div>
-
         <div class="clearfix"></div>
-
         <LtiAuth />
-        <VideoDrawer />
     </div>
 </template>
 
 <script setup>
-import { useStore } from 'vuex';
 import LtiAuth from '@/components/LtiAuth';
 import VideoDrawer from '@/components/Videos/VideoDrawer.vue';
-
-const store = useStore();
 </script>
