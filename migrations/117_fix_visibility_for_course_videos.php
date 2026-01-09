0bf26bfaef3bb234d5dd4cfe849e2f8f432a89e6
Till <tgloeggl@uos.de>
2026-01-09 10:18:17 +0100

    always correctly set visibility in database (#1449)
    * Fix visible_timestamp for videos in playlists
    
    * Always set visibility, visible_timestamp
    
    * fix typo

---
+++ migrations/117_fix_visibility_for_course_videos.php
@@ -0,0 +1,30 @@
+<?php
+
+use Opencast\Models\Workflow;
+
+class FixVisibilityForCourseVideos extends Migration
+{
+    public function description()
+    {
+        return 'Fix visibility for videos in course playlists';
+    }
+
+    public function up()
+    {
+        $db = DBManager::get();
+
+        $db->exec("ALTER TABLE `oc_playlist_seminar_video`
+            CHANGE `visible_timestamp` `visible_timestamp` timestamp NULL
+        ");
+
+        $db->exec("UPDATE `oc_playlist_seminar_video`
+            SET `visible_timestamp` = NULL
+            WHERE `visible_timestamp` = '0000-00-00 00:00:00'
+        ");
+    }
+
+    public function down()
+    {
+
+    }
+}
